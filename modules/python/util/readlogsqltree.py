dc1feb8c429ce0886e3f5762fa4aa7b96962e033
common <common@carnivore.it>
2010-01-06 22:39:29 +0000

    dionaea  - python    * give the readlogsqltree.y script some more love      + commandline args to filter for timespans, things like -t 1262817552-24*3600*7 -T 1262817552 work      + filter for url content and others      + resolve and print rpc servicename and remote procedure name

--- modules/python/util/readlogsqltree.py
+++ modules/python/util/readlogsqltree.py
@@ -1,3 +1,6 @@
+#!/opt/dionaea/bin/python3.1
+
+from optparse import OptionParser
 import sqlite3
 import json
 import sys
@@ -38,18 +41,48 @@ def print_p0fs(cursor, connection, indent):
 		print("%*s p0f: genre:'%s' detail:'%s' uptime:'%i' tos:'%s' dist:'%i' nat:'%i' fw:'%i'" % ( indent, " ", p0f['p0f_genre'], p0f['p0f_detail'], p0f['p0f_uptime'], p0f['p0f_tos'], p0f['p0f_dist'], p0f['p0f_nat'], p0f['p0f_fw']) ) 
 
 def print_dcerpcbinds(cursor, connection, indent):
-	r = cursor.execute("SELECT * from dcerpcbinds WHERE connection = ?", (connection, ))
+	r = cursor.execute("""
+		SELECT 
+			dcerpcbind_uuid,
+			dcerpcservice_name,
+			dcerpcbind_transfersyntax
+		FROM
+			dcerpcbinds 
+			JOIN dcerpcservices ON (dcerpcbind_uuid = dcerpcservice_uuid)
+		WHERE 
+			connection = ?""", (connection, ))
 	dcerpcbinds = resolve_result(r)
 	for dcerpcbind in dcerpcbinds:
-		print("%*s dcerpc bind: uuid '%s' transfersyntax %s" % ( indent, " ", dcerpcbind['dcerpcbind_uuid'], dcerpcbind['dcerpcbind_transfersyntax']) )
+		print("%*s dcerpc bind: uuid '%s' (%s) transfersyntax %s" % ( 
+			indent, " ", 
+			dcerpcbind['dcerpcbind_uuid'], 
+			dcerpcbind['dcerpcservice_name'],
+			dcerpcbind['dcerpcbind_transfersyntax']) )
 
 
 def print_dcerpcrequests(cursor, connection, indent):
-	r = cursor.execute("SELECT * from dcerpcrequests WHERE connection = ?", (connection, ))
+	r = cursor.execute("""
+		SELECT 
+			dcerpcrequest_uuid,
+			dcerpcservice_name,
+			dcerpcrequest_opnum,
+			dcerpcserviceop_name,
+			dcerpcserviceop_vuln
+		FROM 
+			dcerpcrequests 
+			JOIN dcerpcservices ON (dcerpcrequest_uuid = dcerpcservice_uuid) 
+			JOIN dcerpcserviceops ON (dcerpcservices.dcerpcservice = dcerpcserviceops.dcerpcservice AND dcerpcrequest_opnum = dcerpcserviceop_opnum)
+		WHERE 
+			connection = ?""", (connection, ))
 	dcerpcrequests = resolve_result(r)
 	for dcerpcrequest in dcerpcrequests:
-		print("%*s dcerpc request: uuid '%s' opnum %i" % ( indent, " ", dcerpcrequest['dcerpcrequest_uuid'], dcerpcrequest['dcerpcrequest_opnum']) )
-
+		print("%*s dcerpc request: uuid '%s' (%s) opnum %i (%s (%s))" % ( 
+			indent, " ", 
+			dcerpcrequest['dcerpcrequest_uuid'], 
+			dcerpcrequest['dcerpcservice_name'], 
+			dcerpcrequest['dcerpcrequest_opnum'], 
+			dcerpcrequest['dcerpcserviceop_name'], 
+			dcerpcrequest['dcerpcserviceop_vuln']) )
 
 def print_connection(c, indent):
 	if c['connection_type'] == 'accept':
@@ -58,8 +91,6 @@ def print_connection(c, indent):
 		print("%*s connection %i %s %s %s %s:%i -> %s/%s:%i" % ( indent, " ", c['connection'], c['connection_protocol'], c['connection_transport'], c['connection_type'], c['local_host'], c['local_port'], c['remote_hostname'], c['remote_host'], c['remote_port']) )
 	elif c['connection_type'] == 'listen':
 		print("%*s connection %i %s %s %s %s:%i" % ( indent, " ", c['connection'], c['connection_protocol'], c['connection_transport'], c['connection_type'], c['local_host'], c['local_port']) )
-
-
 	
 
 def recursive_print(cursor, connection, indent):
@@ -75,24 +106,74 @@ def recursive_print(cursor, connection, indent):
 		recursive_print(cursor, c['connection'], indent+2)
 		
 
-def print_db(file):
-	dbh = sqlite3.connect(file)
+def print_db(opts, args):
+	dbh = sqlite3.connect(args[0])
 	cursor = dbh.cursor()
 
-	result = cursor.execute("SELECT * from connections WHERE(connection_root = connection OR connection_root IS NULL) AND remote_host = ?", (sys.argv[2],))
+	query = """
+SELECT 
+	c.connection,
+	connection_type,
+	connection_protocol,
+	connection_transport,
+	local_host,
+	local_port,
+	remote_host,
+	remote_hostname,
+	remote_port 
+FROM 
+	connections AS c
+	LEFT OUTER JOIN offers ON (offers.connection = c.connection)
+	LEFT OUTER JOIN downloads ON (downloads.connection = c.connection)
+WHERE
+	(c.connection_root = c.connection OR c.connection_root IS NULL)
+"""
+
+	if options.remote_host:
+		query = query + "\tAND remote_host = %s \n" % options.remote_host
+
+	if options.connection:
+		query = query + "\tAND c.connection = %i \n" % options.connection
+	
+	if options.in_offer_url:
+		query = query + "\tAND offer_url LIKE '%%%s%%' \n" % options.in_offer_url
+
+	if options.in_download_url:
+		query = query + "\tAND download_url LIKE '%%%s%%' \n" % options.in_download_url
+
+	if options.time_from:
+		query = query + "\tAND connection_timestamp > %s \n" % options.time_from
+
+	if options.time_to:
+		query = query + "\tAND connection_timestamp < %s \n" % options.time_to
+
+	print(query)
+	if options.query:
+		return
+
+	result = cursor.execute(query)
 	connections = resolve_result(result)
 
 	for c in connections:
 		connection = c['connection']
 		print_connection(c, 1)
-		print_dcerpcrequests(cursor, c['connection'], 2)
-		print_dcerpcbinds(cursor, c['connection'], 2)
 		print_p0fs(cursor, c['connection'], 2)
+		print_dcerpcbinds(cursor, c['connection'], 2)
+		print_dcerpcrequests(cursor, c['connection'], 2)
+		print_profiles(cursor, c['connection'], 2)
 		print_offers(cursor, c['connection'], 2)
 		print_downloads(cursor, c['connection'], 2)
-		print_profiles(cursor, c['connection'], 2)
 		print_services(cursor, c['connection'], 2)
 		recursive_print(cursor, c['connection'], 2)
 
-#print_db("/opt/dionaea/var/dionaea/logsql.sqlite")
-print_db(sys.argv[1])
+if __name__ == "__main__":
+		parser = OptionParser()
+		parser.add_option("-r", "--remote-host", action="store", type="string", dest="remote_host")
+		parser.add_option("-o", "--in-offer-url", action="store", type="string", dest="in_offer_url")
+		parser.add_option("-d", "--in-download-url", action="store", type="string", dest="in_download_url")
+		parser.add_option("-c", "--connection", action="store", type="int", dest="connection")
+		parser.add_option("-q", "--query-only", action="store_true", dest="query", default=False)
+		parser.add_option("-t", "--time-from", action="store", type="string", dest="time_from")
+		parser.add_option("-T", "--time-to", action="store", type="string", dest="time_to")
+		(options, args) = parser.parse_args()
+		print_db(options, args)
