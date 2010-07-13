0c87baa06d3ae5a120af1d858233924eb11d6a48
Tobi W <twu200@gmail.com>
2010-07-13 14:52:35 +1200

    Basic SIP class with test logging line

--- modules/python/scripts/sip.py
+++ modules/python/scripts/sip.py
@@ -40,3 +40,171 @@ logger.setLevel(logging.DEBUG)
 
 # Shortcut to sip config
 g_sipconfig = g_dionaea.config()['modules']['python']['sip']
+
+def hash(s):
+	return hashlib.md5(s.encode('utf-8')).hexdigest()
+
+TRYING                      = '100'
+RINGING                     = '180'
+CALL_FWD                    = '181'
+QUEUED                      = '182'
+PROGRESS                    = '183'
+OK                          = '200'
+ACCEPTED                    = '202'
+MULTI_CHOICES               = '300'
+MOVED_PERMANENTLY           = '301'
+MOVED_TEMPORARILY           = '302'
+SEE_OTHER					= '303'
+USE_PROXY                   = '305'
+ALT_SERVICE                 = '380'
+BAD_REQUEST                 = '400'
+UNAUTHORIZED                = '401'
+PAYMENT_REQUIRED            = '402'
+FORBIDDEN                   = '403'
+NOT_FOUND                   = '404'
+NOT_ALLOWED                 = '405'
+NOT_ACCEPTABLE              = '406'
+PROXY_AUTH_REQUIRED         = '407'
+REQUEST_TIMEOUT             = '408'
+CONFLICT                    = '409'
+GONE                        = '410'
+LENGTH_REQUIRED             = '411'
+ENTITY_TOO_LARGE            = '413'
+URI_TOO_LARGE               = '414'
+UNSUPPORTED_MEDIA           = '415'
+UNSUPPORTED_URI				= '416'
+BAD_EXTENSION               = '420'
+EXTENSION_REQUIRED			= '421'
+INTERVAL_TOO_BRIEF			= '423'
+NOT_AVAILABLE               = '480'
+NO_TRANSACTION              = '481'
+LOOP                        = '482'
+TOO_MANY_HOPS               = '483'
+ADDRESS_INCOMPLETE          = '484'
+AMBIGUOUS                   = '485'
+BUSY_HERE                   = '486'
+CANCELLED                   = '487'
+NOT_ACCEPTABLE_HERE         = '488'
+REQUEST_PENDING				= '491'
+UNDECIPHERABLE				= '493'
+INTERNAL_ERROR              = '500'
+NOT_IMPLEMENTED             = '501'
+BAD_GATEWAY                 = '502'
+UNAVAILABLE                 = '503'
+GATEWAY_TIMEOUT             = '504'
+SIP_VERSION_NOT_SUPPORTED   = '505'
+MESSAGE_TOO_LARGE			= '513'
+BUSY_EVERYWHERE             = '600'
+DECLINE                     = '603'
+DOES_NOT_EXIST              = '604'
+NOT_ACCEPTABLE_6xx          = '606'
+
+# SIP Responses from SIP Demystified by Gonzalo Camarillo
+RESPONSE = { 
+	# 1xx
+	TRYING:                     '100 Trying',
+	RINGING:                    '180 Ringing',
+	CALL_FWD:                   '181 Call is being forwarded',
+	QUEUED:                     '182 Queued',
+	PROGRESS:                   '183 Session progress',
+
+	# 2xx
+	OK:                         '200 OK',
+	ACCEPTED:                   '202 Accepted',
+
+	# 3xx
+	MULTI_CHOICES:              '300 Multiple choices',
+	MOVED_PERMANENTLY:          '301 Moved permanently',
+	MOVED_TEMPORARILY:          '302 Moved temporarily',
+	SEE_OTHER:					'303 See other',
+	USE_PROXY:                  '305 Use proxy',
+	ALT_SERVICE:                '380 Alternative service',
+
+	# 4xx
+	BAD_REQUEST:                '400 Bad request',
+	UNAUTHORIZED:               '401 Unauthorized',
+	PAYMENT_REQUIRED:           '402 Payment required',
+	FORBIDDEN:                  '403 Forbidden',
+	NOT_FOUND:                  '404 Not found',
+	NOT_ALLOWED:                '405 Method not allowed',
+	NOT_ACCEPTABLE:             '406 Not acceptable',
+	PROXY_AUTH_REQUIRED:        '407 Proxy authentication required',
+	REQUEST_TIMEOUT:            '408 Request time-out',
+	CONFLICT:                   '409 Conflict',
+	GONE:                       '410 Gone',
+	LENGTH_REQUIRED:            '411 Length required',
+	ENTITY_TOO_LARGE:           '413 Request entity too large',
+	URI_TOO_LARGE:              '414 Request-URI too large',
+	UNSUPPORTED_MEDIA:          '415 Unsupported media type',
+	UNSUPPORTED_URI:			'416 Unsupported URI scheme',
+	BAD_EXTENSION:              '420 Bad extension',
+	EXTENSION_REQUIRED:			'421 Extension required',
+	INTERVAL_TOO_BRIEF:			'423 Interval too brief',
+	NOT_AVAILABLE:              '480 Temporarily not available',
+	NO_TRANSACTION:             '481 Call leg/transaction does not exist',
+	LOOP:                       '482 Loop detected',
+	TOO_MANY_HOPS:              '483 Too many hops',
+	ADDRESS_INCOMPLETE:         '484 Address incomplete',
+	AMBIGUOUS:                  '485 Ambiguous',
+	BUSY_HERE:                  '486 Busy here',
+	CANCELLED:                  '487 Request cancelled',
+	NOT_ACCEPTABLE_HERE:        '488 Not acceptable here',
+	REQUEST_PENDING:			'491 Request pending',
+	UNDECIPHERABLE:				'493 Undecipherable',
+
+	# 5xx
+	INTERNAL_ERROR:             '500 Internal server error',
+	NOT_IMPLEMENTED:            '501 Not implemented',
+	BAD_GATEWAY:                '502 Bad gateway',
+	UNAVAILABLE:                '503 Service unavailable',
+	GATEWAY_TIMEOUT:            '504 Gateway time-out',
+	SIP_VERSION_NOT_SUPPORTED:  '505 SIP version not supported',
+	MESSAGE_TOO_LARGE:			'513 Message too large',
+
+	# 6xx
+	BUSY_EVERYWHERE:            '600 Busy everywhere',
+	DECLINE:                    '603 Decline',
+	DOES_NOT_EXIST:             '604 Does not exist anywhere',
+	NOT_ACCEPTABLE_6xx:         '606 Not acceptable'
+}
+
+# SIP headers have short forms
+shortHeaders = {"call-id": "i",
+                "contact": "m",
+                "content-encoding": "e",
+                "content-length": "l",
+                "content-type": "c",
+                "from": "f",
+                "subject": "s",
+                "to": "t",
+                "via": "v",
+				"cseq": "cseq",
+				"accept": "accept",
+				"user-agent": "user-agent",
+				"max-forwards": "max-forwards",
+				"www-authentication": "www-authentication",
+				"authorization": "authorization"
+                }
+
+longHeaders = {}
+for k, v in shortHeaders.items():
+    longHeaders[v] = k
+del k, v
+
+class SipParsingError(Exception):
+	"""Exception class for errors occuring during SIP message parsing"""
+
+class AuthenticationError(Exception):
+	"""Exception class for errors occuring during SIP authentication"""
+
+class Sip(connection):
+	"""Only UDP connections are supported at the moment"""
+
+    def __init__(self):
+    connection.__init__(self, 'udp')
+
+    # Dictionary with SIP sessions (key is Call-ID)
+    self.__sessions = {}
+
+    # Test log entry
+    logger.info("SIP instance created")
