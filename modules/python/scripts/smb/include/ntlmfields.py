DinoTools/dionaea

commit c7af3ee9c3fa432b5f62573dc1cd16a51c0d0333

Changes on NTLM reply and Treeconnect_AndX_Response_Extended() layer added -Set NTLM_Version to Windows 5.1 Build 2600 NLMPv15 -NTLM_Challenge : Change TargetFieldString to ConditionalField, Prepare AVPair for further development -Correction to support NTLMSSP_Request_Target -Treeconnect_AndX_Response_Extended() layer added

modules/python/scripts/smb/include/ntlmfields.py
@@ -134,11 +134,12 @@ class NTLMSSP_Message_Signature(Packet):
 class NTLM_Version(Packet):
 	name = "NTLM Version"
 	fields_desc = [
+		# Set to Windows 5.1 Build 2600 NLMPv15
 		ByteField("ProductMajorVersion",5),
 		ByteField("ProductMinorVersion",1),
-		LEShortField("ProductBuild",0),
+		LEShortField("ProductBuild",2600),
 		StrFixedLenField("Reserved", "\0\0\0", 3),
-		ByteField("NTLMRevisionCurrent",1),
+		ByteField("NTLMRevisionCurrent",15),
 	]
 
 AV_Pair_Ids = {
@@ -158,7 +159,7 @@ AV_Pair_Ids = {
 class AV_PAIR(Packet):
 	name = "AV Pair"
 	fields_desc = [
-		ShortEnumField("Id",0,AV_Pair_Ids),
+		LEShortEnumField("Id",0,AV_Pair_Ids),
 		FieldLenField("Len", 0, fmt='<H', length_of="Value"),
 		StrLenField("Value", "", length_from=lambda x:x.Len),
 	]
@@ -197,7 +198,14 @@ class NTLM_Challenge(Packet):
 		PacketField("TargetInfoFields",NTLM_Value(),NTLM_Value),
 #		PacketField("Version",0,NTLM_Version),
 		ConditionalField(PacketField("Version",NTLM_Version(),NTLM_Version), lambda x: x.NegotiateFlags & NTLMSSP_NEGOTIATE_VERSION),
-		StrField("Payload","")
+		ConditionalField(StrFixedLenField("TargetFieldString","HOMEUSER-3AF6FE".encode('utf16')[2:],30), lambda x: x.NegotiateFlags & NTLMSSP_REQUEST_TARGET),
+		#StrField("Payload","")
+		#PacketField("AVPair1",AV_PAIR(),AV_PAIR),
+		#PacketField("AVPair2",AV_PAIR(),AV_PAIR),
+		#PacketField("AVPair3",AV_PAIR(),AV_PAIR),
+		#PacketField("AVPair4",AV_PAIR(),AV_PAIR),
+		#PacketField("AVPair5",AV_PAIR(),AV_PAIR),
+		#PacketField("AVPair6",AV_PAIR(),AV_PAIR),
 	]
 
 class NTLM_Authenticate(Packet):

modules/python/scripts/smb/smb.py
@@ -38,7 +38,7 @@ from uuid import UUID
 from .include.smbfields import *
 
 from .include.gssapifields import GSSAPI,SPNEGO, NegTokenTarg
-from .include.ntlmfields import NTLMSSP_Header, NTLM_Negotiate, NTLM_Challenge
+from .include.ntlmfields import NTLMSSP_Header, NTLM_Negotiate, NTLM_Challenge, NTLMSSP_REQUEST_TARGET
 from .include.packet import Raw
 from .include.asn1.ber import BER_len_dec, BER_len_enc, BER_identifier_dec, BER_CLASS_APP, BER_CLASS_CON,BER_identifier_enc
 
@@ -204,6 +204,11 @@ class smbd(connection):
 						ntlmnegotiate = ntlmssp.getlayer(NTLM_Negotiate)
 						rntlmssp = NTLMSSP_Header(MessageType=2)
 						rntlmchallenge = NTLM_Challenge(NegotiateFlags=ntlmnegotiate.NegotiateFlags)
+						if ntlmnegotiate.NegotiateFlags & NTLMSSP_REQUEST_TARGET:
+							rntlmchallenge.TargetNameFields.Offset = 0x38
+							rntlmchallenge.TargetNameFields.Len = 0x1E
+							rntlmchallenge.TargetNameFields.MaxLen = 0x1E
+						
 						rntlmchallenge.ServerChallenge = b"\xa4\xdf\xe8\x0b\xf5\xc6\x1e\x3a"
 						rntlmssp = rntlmssp / rntlmchallenge
 						rntlmssp.show()
@@ -244,7 +249,10 @@ class smbd(connection):
 							ntlmnegotiate = ntlmssp.getlayer(NTLM_Negotiate)
 							rntlmssp = NTLMSSP_Header(MessageType=2)
 							rntlmchallenge = NTLM_Challenge(NegotiateFlags=ntlmnegotiate.NegotiateFlags)
-							rntlmchallenge.TargetInfoFields.Offset = rntlmchallenge.TargetNameFields.Offset = 0x30
+							if ntlmnegotiate.NegotiateFlags & NTLMSSP_REQUEST_TARGET:
+								rntlmchallenge.TargetNameFields.Offset = 0x38
+								rntlmchallenge.TargetNameFields.Len = 0x1E
+								rntlmchallenge.TargetNameFields.MaxLen = 0x1E
 							rntlmchallenge.ServerChallenge = b"\xa4\xdf\xe8\x0b\xf5\xc6\x1e\x3a"
 							rntlmssp = rntlmssp / rntlmchallenge
 							rntlmssp.show()
@@ -277,6 +285,10 @@ class smbd(connection):
 			r = SMB_Treeconnect_AndX_Response()
 			h = p.getlayer(SMB_Treeconnect_AndX_Request)
 			print ("Service : %s" % h.Path)
+			
+			# for SMB_Treeconnect_AndX_Request.Flags = 0x0008
+			if h.Flags == 0x08:
+				r = SMB_Treeconnect_AndX_Response_Extended()
 			
 			# specific for NMAP smb-enum-shares.nse support
 			if h.Path == b'nmap-share-test\0':

modules/python/scripts/smb/include/smbfields.py
@@ -902,6 +902,25 @@ class SMB_Treeconnect_AndX_Response(Packet):
 		StrNullField("NativeFilesystem",""),
 	]
 
+#[MS-SMB].pdf
+#Page 62
+class SMB_Treeconnect_AndX_Response_Extended(Packet):
+	name="SMB Treeconnect AndX Response Extended"
+	smb_cmd = SMB_COM_TREE_CONNECT_ANDX #0x75
+	fields_desc = [
+		ByteField("WordCount",7),
+		ByteEnumField("AndXCommand",0xff,SMB_Commands),
+		ByteField("Reserved1",0),
+		LEShortField("AndXOffset",56), #windows xp gives senseless 56
+		XLEShortField("OptSupport",0x0001),
+		FlagsField("MaximalShareAccessRights", 0x01ff, -32, SMB_AccessMask),
+		FlagsField("GuestMaximalShareAccessRights", 0x01ff, -32, SMB_AccessMask),
+		LEShortField("ByteCount",7),
+		StrNullField("Service","IPC"),
+		ConditionalField(StrFixedLenField("Padding", b'\0', 2), lambda x:x.underlayer.Flags2 & SMB_FLAGS2_UNICODE),
+		StrNullField("NativeFileSystem",""),	
+	]
+
 # Used when the error's return is needed
 class SMB_Treeconnect_AndX_Response2(Packet):
 	name="SMB Treeconnect AndX Response2"
@@ -1406,6 +1425,7 @@ bind_bottom_up(SMB_Header, SMB_Treedisconnect, Command=lambda x: x==0x71)
 bind_bottom_up(SMB_Header, SMB_Treeconnect_AndX_Request, Command=lambda x: x==0x75, Flags=lambda x: not x&0x80)
 bind_bottom_up(SMB_Header, SMB_Treeconnect_AndX_Response, Command=lambda x: x==0x75, Flags=lambda x: x&0x80)
 bind_bottom_up(SMB_Header, SMB_Treeconnect_AndX_Response2, Command=lambda x: x==0x75, Flags=lambda x: x&0x80)
+#bind_bottom_up(SMB_Header, SMB_Treeconnect_AndX_Response_Extended, Command=lambda x: x==0x75, Flags=lambda x: x&0x80)
 bind_bottom_up(SMB_Header, SMB_NTcreate_AndX_Request, Command=lambda x: x==0xa2, Flags=lambda x: not x&0x80)
 bind_bottom_up(SMB_Header, SMB_NTcreate_AndX_Response, Command=lambda x: x==0xa2, Flags=lambda x: x&0x80)
 bind_bottom_up(SMB_Header, SMB_Trans_Request, Command=lambda x: x==0x25, Flags=lambda x: not x&0x80)
@@ -1444,6 +1464,7 @@ bind_top_down(SMB_Header, SMB_Sessionsetup_AndX_Response2, Command=0x73)
 bind_top_down(SMB_Header, SMB_Sessionsetup_ESEC_AndX_Response, Command=0x73)
 bind_top_down(SMB_Header, SMB_Treeconnect_AndX_Response, Command=0x75)
 bind_top_down(SMB_Header, SMB_Treeconnect_AndX_Response2, Command=0x75)
+#bind_top_down(SMB_Header, SMB_Treeconnect_AndX_Response_Extended, Command=0x75)
 bind_top_down(SMB_Header, SMB_Treedisconnect, Command=0x71)
 bind_top_down(SMB_Header, SMB_NTcreate_AndX_Response, Command=0xa2)
 bind_top_down(SMB_Header, SMB_Write_AndX_Response, Command=0x2f)

commit ac117d6d99320d06aa7d37dfbcde47cde46e241f

python - smb  - fix regression introduced by c7af3ee9c3fa432b5f62573dc1cd16a51c0d0333    NTLM authentication was broken due to a bad NTLM_Challenge response    smbclient would complain with 'Failed to parse the NTLMSSP Challenge: (#1)'    The NTLM_Challenge could be improved by adding    some payload to the TargetInfoField,    but that has to be done in a very different way, the length & offset have to be calculated for    TargetNameFields & TargetField, and the actual strings have to go into Payload    So far this is only required for the AT remote commands, which do not work due to lack of netbios anyway.    Therefore return to status quo

modules/python/scripts/smb/include/ntlmfields.py
@@ -198,8 +198,8 @@ class NTLM_Challenge(Packet):
 		PacketField("TargetInfoFields",NTLM_Value(),NTLM_Value),
 #		PacketField("Version",0,NTLM_Version),
 		ConditionalField(PacketField("Version",NTLM_Version(),NTLM_Version), lambda x: x.NegotiateFlags & NTLMSSP_NEGOTIATE_VERSION),
-		ConditionalField(StrFixedLenField("TargetFieldString","HOMEUSER-3AF6FE".encode('utf16')[2:],30), lambda x: x.NegotiateFlags & NTLMSSP_REQUEST_TARGET),
-		#StrField("Payload","")
+#		ConditionalField(StrFixedLenField("TargetFieldString","HOMEUSER-3AF6FE".encode('utf16')[2:],30), lambda x: x.NegotiateFlags & NTLMSSP_REQUEST_TARGET),
+		StrField("Payload","")
 		#PacketField("AVPair1",AV_PAIR(),AV_PAIR),
 		#PacketField("AVPair2",AV_PAIR(),AV_PAIR),
 		#PacketField("AVPair3",AV_PAIR(),AV_PAIR),

modules/python/scripts/smb/smb.py
@@ -204,10 +204,10 @@ class smbd(connection):
 						ntlmnegotiate = ntlmssp.getlayer(NTLM_Negotiate)
 						rntlmssp = NTLMSSP_Header(MessageType=2)
 						rntlmchallenge = NTLM_Challenge(NegotiateFlags=ntlmnegotiate.NegotiateFlags)
-						if ntlmnegotiate.NegotiateFlags & NTLMSSP_REQUEST_TARGET:
-							rntlmchallenge.TargetNameFields.Offset = 0x38
-							rntlmchallenge.TargetNameFields.Len = 0x1E
-							rntlmchallenge.TargetNameFields.MaxLen = 0x1E
+#						if ntlmnegotiate.NegotiateFlags & NTLMSSP_REQUEST_TARGET:
+#							rntlmchallenge.TargetNameFields.Offset = 0x38
+#							rntlmchallenge.TargetNameFields.Len = 0x1E
+#							rntlmchallenge.TargetNameFields.MaxLen = 0x1E
 						
 						rntlmchallenge.ServerChallenge = b"\xa4\xdf\xe8\x0b\xf5\xc6\x1e\x3a"
 						rntlmssp = rntlmssp / rntlmchallenge
@@ -249,10 +249,11 @@ class smbd(connection):
 							ntlmnegotiate = ntlmssp.getlayer(NTLM_Negotiate)
 							rntlmssp = NTLMSSP_Header(MessageType=2)
 							rntlmchallenge = NTLM_Challenge(NegotiateFlags=ntlmnegotiate.NegotiateFlags)
-							if ntlmnegotiate.NegotiateFlags & NTLMSSP_REQUEST_TARGET:
-								rntlmchallenge.TargetNameFields.Offset = 0x38
-								rntlmchallenge.TargetNameFields.Len = 0x1E
-								rntlmchallenge.TargetNameFields.MaxLen = 0x1E
+							rntlmchallenge.TargetInfoFields.Offset = rntlmchallenge.TargetNameFields.Offset = 0x30
+#							if ntlmnegotiate.NegotiateFlags & NTLMSSP_REQUEST_TARGET:
+#								rntlmchallenge.TargetNameFields.Offset = 0x38
+#								rntlmchallenge.TargetNameFields.Len = 0x1E
+#								rntlmchallenge.TargetNameFields.MaxLen = 0x1E
 							rntlmchallenge.ServerChallenge = b"\xa4\xdf\xe8\x0b\xf5\xc6\x1e\x3a"
 							rntlmssp = rntlmssp / rntlmchallenge
 							rntlmssp.show()