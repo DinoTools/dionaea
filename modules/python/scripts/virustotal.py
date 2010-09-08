3bd284555e7a4857563c2bb2bbff929cbacd326a
common <common@carnivore.it>
2010-09-08 20:01:25 +0200

    virustotal - more comment code  - as vt keeps rejecting 95% of the comments with 'internal server error',    retry posting the comment, lets see how long it takes in cases where it fails

--- modules/python/scripts/virustotal.py
+++ modules/python/scripts/virustotal.py
@@ -3,24 +3,35 @@ from dionaea.core import ihandler, incident, g_dionaea
 import logging
 import json
 import os
+import uuid
 from dionaea import pyev
 
 logger = logging.getLogger('virustotal')
 logger.setLevel(logging.DEBUG)
+
+class vtreport:
+	def __init__(self, md5hash, file):
+		self.md5hash = md5hash
+		self.file = file
+		self.comment_timer = None
+		self.comment_retries = 0
+
 
 class virustotalhandler(ihandler):
 	def __init__(self, path):
 		logger.debug("%s ready!" % (self.__class__.__name__))
 		ihandler.__init__(self, path)
 		self.apikey = g_dionaea.config()['modules']['python']['virustotal']['apikey']
-		self.polling = {}
-		self.comments = {}
+		self.cookies = {}
 		self.loop = pyev.default_loop()
 	
 	def handle_incident(self, icd):
 		pass
 
 	def handle_incident_dionaea_download_complete_unique(self, icd):
+		cookie = str(uuid.uuid4())
+		self.cookies[cookie] = vtreport(icd.md5hash, icd.file)
+
 		i = incident("dionaea.upload.request")
 		i.url = "http://www.virustotal.com/api/get_file_report.json"
 		
@@ -28,7 +39,7 @@ class virustotalhandler(ihandler):
 		i.key = self.apikey
 
 		i._callback = "dionaea.modules.python.virustotal.get_file_report"
-		i._userdata = icd.md5hash
+		i._userdata = cookie
 
 		i.report()
 
@@ -36,32 +47,33 @@ class virustotalhandler(ihandler):
 		f = open(icd.path, mode='r')
 		j = json.load(f)
 
-		md5hash = icd._userdata
+		cookie = icd._userdata
+		vtr = self.cookies[cookie]
 
 		if j['result'] == -1:
 			logger.warn("something is wrong with your virustotal api key")
 
 		elif j['result'] == 0:
-			logger.warn("file {} is unknown to virus total, uploading".format(md5hash) )
+			logger.warn("file {} is unknown to virus total, uploading".format(vtr.md5hash) )
 
 			i = incident("dionaea.upload.request")
 			i.url = "http://www.virustotal.com/api/scan_file.json"
 			i.key = self.apikey
 
-			i.set('file://file', '/opt/dionaea/var/dionaea/binaries/' + md5hash)
-			i.file_fieldname = "file"
+			i.set('file://file', vtr.file)
 			i._callback = "dionaea.modules.python.virustotal_scan_file"
-			i._userdata = md5hash
+			i._userdata = cookie
 			i.report()
 
 		elif j['result'] == 1:
+			logger.debug("report {}".format(j) )
 			date = j['report'][0]
 			scans = j['report'][1]
 			for av in scans:
 				logger.debug("scanner {} result {}".format(av,scans[av]))
 
 			i = incident("dionaea.module.python.modules.virustotal.report")
-			i.md5hash = md5hash
+			i.md5hash = vtr.md5hash
 			i.path = icd.path
 			i.report()
 
@@ -70,36 +82,51 @@ class virustotalhandler(ihandler):
 		f = open(icd.path, mode='r')
 		j = json.load(f)
 		logger.debug("scan_file {}".format(j))
+		cookie = icd._userdata
+		vtr = self.cookies[cookie]
 
 		if j['result'] == 1:
-			timer = pyev.Timer(60, 0, self.loop, self.__handle_comment_timeout)
-			timer.data = icd._userdata
-			self.comments[timer] = True
-			timer.start()
+			vtr.comment_timer = pyev.Timer(60, 0, self.loop, self.__handle_comment_timeout)
+			vtr.comment_timer.data = cookie
+			vtr.comment_timer.start()
 		
 	def __handle_comment_timeout(self, watcher, event):
-		md5hash = watcher.data
-		del self.comments[watcher] 
+		cookie = watcher.data
+		vtr = self.cookies[cookie]
+		vtr.comment_retries += 1
 
 		i = incident("dionaea.upload.request")
 		i.url = "http://www.virustotal.com/api/make_comment.json"
 		i.key = self.apikey
-		i.file = md5hash
+		i.file = vtr.md5hash
 		i.tags = "honeypot;malware;networkworm"
-		i.comment = "This sample was captured in the wild and uploaded by the dionaea honeypot."
+		i.comment = "This sample was captured in the wild and uploaded by the dionaea honeypot, still testing comments ..."
 		i._callback = "dionaea.modules.python.virustotal_make_comment"
-		i._userdata = md5hash
+		i._userdata = cookie
 		i.report()
 
 	def handle_incident_dionaea_modules_python_virustotal_make_comment(self, icd):
+		cookie = icd._userdata
+		vtr = self.cookies[cookie]
 		f = open(icd.path, mode='r')
 		try:
 			j = json.load(f)
+			logger.debug("posting comment success (try {}) ".format(vtr.comment_retries))
 		except Exception as e:
 			f.seek(0, os.SEEK_SET)
 			msg = f.readline(1024)
 			logger.warn("posting comment failed, server returned '{}'".format(msg))
+			if vtr.comment_retries > 10:
+				logger.warn("posting comment failed {:i} times, giving up".format(vtr.comment_retries))
+				del self.cookies[cookie]
+				return
+			
+			vtr.comment_timer = pyev.Timer(60, 0, self.loop, self.__handle_comment_timeout)
+			vtr.comment_timer.data = cookie
+			vtr.comment_timer.start()
 			return
+
 		logger.debug("comment {}".format(j))
+		del self.cookies[cookie]
 
 
