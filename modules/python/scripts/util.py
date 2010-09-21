b0c6847c3476e3858489f3c236a9990a3f94d4c3
rep <ms@mwcollect.org>
2010-09-21 21:21:05 +0200

    python  - add sha512file to utils

--- modules/python/scripts/util.py
+++ modules/python/scripts/util.py
@@ -2,8 +2,13 @@ import sys
 import hashlib
 
 def md5file(filename):
+	return hashfile(filename, hashlib.md5())
+
+def sha512file(filename):
+	return hashfile(filename, hashlib.sha512())
+
+def hashfile(filename, digest):
     fh = open(filename, mode="rb")
-    digest = hashlib.md5()
     while 1:
         buf = fh.read(4096)
         if len(buf) == 0:
