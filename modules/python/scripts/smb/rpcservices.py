abdaf03791d146c622e0340e3bbea55fbb4c659f
gento <gento@local.(none)>
2010-09-23 16:08:13 +0800

    contributor's name added

--- modules/python/scripts/smb/rpcservices.py
+++ modules/python/scripts/smb/rpcservices.py
@@ -4,7 +4,7 @@
 #*
 #*
 #*
-#* Copyright (C) 2009  Paul Baecher & Markus Koetter & Mark Schloesser
+#* Copyright (C) 2009  Paul Baecher & Markus Koetter & Mark Schloesser & Tan Kean Siong
 #* 
 #* This program is free software; you can redistribute it and/or
 #* modify it under the terms of the GNU General Public License
