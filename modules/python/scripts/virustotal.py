45616ca2c9180c5d768c45e03a853c71f5652ff5
common <common@carnivore.it>
2010-10-03 18:10:00 +0200

    virustotal - honor the vt api limit  - this is pretty complicated, vt has a limit of 20 requests in 5 minutes    but we do not control how much files we get    so we have to store the information in a database,    and query virustotal with a request all 20 seconds  - the database is sqlite, and we use it to store everything we need to interact with vt    if we get a new file, it gets into the database    vt gets queried for the new file    the result shows if vt knew the file before    if vt knew the file before, we are done, and can remove the file from the db    if vt did not know the file before, we mark the file as "submit", so it gets submitted    once the file is submitted successfully, we mark the file to be queried for the scanner results    once the scanner results are received, we mark the file to post a comment,    once we received the comment, we remove the file from the db  - Some benefits from this procedure,    * you can easily query vt for files you received before    * querying vt survives stopping/restarting the service    * less timers

--- modules/python/scripts/virustotal.py
+++ modules/python/scripts/virustotal.py
@@ -31,18 +31,18 @@ import logging
 import json
 import os
 import uuid
+import sqlite3
 from dionaea import pyev
 
 logger = logging.getLogger('virustotal')
 logger.setLevel(logging.DEBUG)
 
 class vtreport:
-	def __init__(self, md5hash, file):
+	def __init__(self, backlogfile, md5hash, file, status):
+		self.backlogfile = backlogfile
 		self.md5hash = md5hash
 		self.file = file
-		self.comment_timer = None
-		self.comment_retries = 0
-
+		self.status = status
 
 class virustotalhandler(ihandler):
 	def __init__(self, path):
@@ -51,31 +51,70 @@ class virustotalhandler(ihandler):
 		self.apikey = g_dionaea.config()['modules']['python']['virustotal']['apikey']
 		self.cookies = {}
 		self.loop = pyev.default_loop()
-	
+
+		self.backlog_timer = pyev.Timer(0, 20, self.loop, self.__handle_backlog_timeout)
+		self.backlog_timer.start()
+
+		self.dbh = sqlite3.connect('/opt/dionaea/var/dionaea/virustotal.sqlite')
+		self.cursor = self.dbh.cursor()
+		self.cursor.execute("""
+			CREATE TABLE IF NOT EXISTS backlogfiles (
+				backlogfile INTEGER PRIMARY KEY,
+				status TEXT NOT NULL, -- new, submit, query, comment
+				md5_hash TEXT NOT NULL,
+				path TEXT NOT NULL,
+				timestamp INTEGER NOT NULL,
+				scan_id TEXT,
+				lastcheck_time INTEGER,
+				submit_time INTEGER
+			);""")
+
+	def __handle_backlog_timeout(self, watcher, event):
+		logger.debug("backlog_timeout")
+
+		# try to comment on files
+		sfs = self.cursor.execute("""SELECT backlogfile, md5_hash, path FROM backlogfiles WHERE status = 'comment' LIMIT 1""")
+		for sf in sfs:
+			self.make_comment(sf[0], sf[1], sf[2], 'comment')
+			return
+
+		# try to receive reports for files we submitted
+		sfs = self.cursor.execute("""SELECT backlogfile, md5_hash, path FROM backlogfiles WHERE status = 'query' AND submit_time < strftime("%s",'now')-15*60 AND lastcheck_time < strftime("%s",'now')-15*60 LIMIT 1""")
+		for sf in sfs:
+			self.get_file_report(sf[0], sf[1], sf[2], 'query')
+			return
+
+		# submit files not known to virustotal
+		sfs = self.cursor.execute("""SELECT backlogfile, md5_hash, path FROM backlogfiles WHERE status = 'submit' LIMIT 1""")
+		for sf in sfs:
+			self.scan_file(sf[0], sf[1], sf[2], 'submit')
+			return
+
+		# query new files
+		sfs = self.cursor.execute("""SELECT backlogfile, md5_hash, path FROM backlogfiles WHERE status = 'new' ORDER BY timestamp DESC LIMIT 1""")
+		for sf in sfs:
+			self.get_file_report(sf[0], sf[1], sf[2], 'new')
+			return
+
 	def stop(self):
-		for c in self.cookies:
-			cookie = self.cookies[c]
-			if cookie.comment_timer is not None:
-				cookie.comment_timer.stop()
-		self.loop = None
-		self.cookies = None
+		self.backlog_timer.stop()
 
 	def handle_incident(self, icd):
 		pass
 
 	def handle_incident_dionaea_download_complete_unique(self, icd):
+		self.cursor.execute("""INSERT INTO backlogfiles (md5_hash, path, status, timestamp) VALUES (?,?,?,strftime("%s",'now')) """, (icd.md5hash, icd.file, 'new'))
+
+	def get_file_report(self, backlogfile, md5_hash, path, status):
 		cookie = str(uuid.uuid4())
-		self.cookies[cookie] = vtreport(icd.md5hash, icd.file)
+		self.cookies[cookie] = vtreport(backlogfile, md5_hash, path, status)
 
 		i = incident("dionaea.upload.request")
 		i._url = "http://www.virustotal.com/api/get_file_report.json"
-		
-		i.resource = icd.md5hash
+		i.resource = md5_hash
 		i.key = self.apikey
-
 		i._callback = "dionaea.modules.python.virustotal.get_file_report"
 		i._userdata = cookie
-
 		i.report()
 
 	def handle_incident_dionaea_modules_python_virustotal_get_file_report(self, icd):
@@ -88,29 +127,40 @@ class virustotalhandler(ihandler):
 		if j['result'] == -1:
 			logger.warn("something is wrong with your virustotal api key")
 
-		elif j['result'] == 0:
-			logger.warn("file {} is unknown to virus total, uploading".format(vtr.md5hash) )
+		elif j['result'] == 0: # file unknown, mark for submit
+			self.cursor.execute("""UPDATE backlogfiles SET status = 'submit', lastcheck_time = strftime("%s",'now') WHERE backlogfile = ?""", (vtr.backlogfile,))
+			self.dbh.commit()
+		elif j['result'] == 1: # file known
+#			self.cursor.execute("""UPDATE backlogfiles SET status = 'comment', lastcheck_time = strftime("%s",'now') WHERE backlogfile = ?""", (vtr.backlogfile,))
+			if vtr.status == 'new':
+				self.cursor.execute("""DELETE FROM backlogfiles WHERE backlogfile = ?""", (vtr.backlogfile,) )
+			elif vtr.status == 'query':
+				self.cursor.execute("""UPDATE backlogfiles SET status = 'comment' WHERE backlogfile = ?) """, (vtr.backlogfile, ))
+			self.dbh.commit()
 
-			i = incident("dionaea.upload.request")
-			i._url = "http://www.virustotal.com/api/scan_file.json"
-			i.key = self.apikey
-
-			i.set('file://file', vtr.file)
-			i._callback = "dionaea.modules.python.virustotal_scan_file"
-			i._userdata = cookie
-			i.report()
-
-		elif j['result'] == 1:
 			logger.debug("report {}".format(j) )
 			date = j['report'][0]
 			scans = j['report'][1]
-			for av in scans:
-				logger.debug("scanner {} result {}".format(av,scans[av]))
+#			for av in scans:
+#				logger.debug("scanner {} result {}".format(av,scans[av]))
 
 			i = incident("dionaea.modules.python.virustotal.report")
 			i.md5hash = vtr.md5hash
 			i.path = icd.path
 			i.report()
+		del self.cookies[cookie]
+
+	def scan_file(self, backlogfile, md5_hash, path, status):
+		cookie = str(uuid.uuid4())
+		self.cookies[cookie] = vtreport(backlogfile, md5_hash, path, status)
+
+		i = incident("dionaea.upload.request")
+		i._url = "http://www.virustotal.com/api/scan_file.json"
+		i.key = self.apikey
+		i.set('file://file', path)
+		i._callback = "dionaea.modules.python.virustotal_scan_file"
+		i._userdata = cookie
+		i.report()
 
 
 	def handle_incident_dionaea_modules_python_virustotal_scan_file(self, icd):
@@ -119,21 +169,23 @@ class virustotalhandler(ihandler):
 		logger.debug("scan_file {}".format(j))
 		cookie = icd._userdata
 		vtr = self.cookies[cookie]
+		
 
 		if j['result'] == 1:
-			vtr.comment_timer = pyev.Timer(60, 0, self.loop, self.__handle_comment_timeout)
-			vtr.comment_timer.data = cookie
-			vtr.comment_timer.start()
-		
-	def __handle_comment_timeout(self, watcher, event):
-		cookie = watcher.data
-		vtr = self.cookies[cookie]
-		vtr.comment_retries += 1
+			scan_id = j['scan_id']
+			# recycle this entry for the query
+			self.cursor.execute("""UPDATE backlogfiles SET scan_id = ?, status = 'query', submit_time = strftime("%s",'now') WHERE backlogfile = ?""", (scan_id, vtr.backlogfile,))
+			self.dbh.commit()
+		del self.cookies[cookie]
+
+	def make_comment(self, backlogfile, md5_hash, path, status):
+		cookie = str(uuid.uuid4())
+		self.cookies[cookie] = vtreport(backlogfile, md5_hash, path, status)
 
 		i = incident("dionaea.upload.request")
 		i._url = "http://www.virustotal.com/api/make_comment.json"
 		i.key = self.apikey
-		i.file = vtr.md5hash
+		i.file = md5_hash
 		i.tags = "honeypot;malware;networkworm"
 		i.comment = "This sample was captured in the wild and uploaded by the dionaea honeypot."
 		i._callback = "dionaea.modules.python.virustotal_make_comment"
@@ -146,22 +198,9 @@ class virustotalhandler(ihandler):
 		f = open(icd.path, mode='r')
 		try:
 			j = json.load(f)
-			logger.debug("posting comment success (try {}) ".format(vtr.comment_retries))
+			self.cursor.execute("""DELETE FROM backlogfiles WHERE backlogfile = ?""", (vtr.backlogfile,))
+			self.dbh.commit()
 		except Exception as e:
-			f.seek(0, os.SEEK_SET)
-			msg = f.readline(1024)
-			logger.warn("posting comment failed, server returned '{}'".format(msg))
-			if vtr.comment_retries > 10:
-				logger.warn("posting comment failed {} times, giving up".format(vtr.comment_retries))
-				del self.cookies[cookie]
-				return
-			
-			vtr.comment_timer = pyev.Timer(60, 0, self.loop, self.__handle_comment_timeout)
-			vtr.comment_timer.data = cookie
-			vtr.comment_timer.start()
-			return
-
-		logger.debug("comment {}".format(j))
+			pass
 		del self.cookies[cookie]
-
 
