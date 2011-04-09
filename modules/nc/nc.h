6486eef467edf36f3f46894293dd784775e456a8
common <common@carnivore.it>
2011-04-09 14:58:16 +0200

    nc - add ab compatible http layer

--- modules/nc/nc.h
+++ modules/nc/nc.h
@@ -40,6 +40,12 @@ bool proto_nc_timeout(struct connection *con, void *context);
 void *proto_nc_ctx_new(struct connection *con);
 void proto_nc_ctx_free(void *ctx);
 
+void proto_nc_established_http(struct connection *con);
+uint32_t proto_nc_io_in_http(struct connection *con, void *context, unsigned char *data, uint32_t size);
+void *proto_nc_ctx_new_http(struct connection *con);
+void proto_nc_ctx_free_http(void *ctx);
+
+extern struct protocol proto_nc_http;
 extern struct protocol proto_nc_source;
 extern struct protocol proto_nc_sink;
 extern struct protocol proto_nc_redir;
