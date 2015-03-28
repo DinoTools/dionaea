991d4945a204f3e27cafe097709e1103d6517cf0
Katarina Durechova <katarina.durechova@nic.cz>
2015-03-28 19:04:29 +0000

    gssapifields - Change tabs to four spaces + delete spaces on EOL

--- modules/python/scripts/smb/include/gssapifields.py
+++ modules/python/scripts/smb/include/gssapifields.py
@@ -5,23 +5,23 @@
 #*
 #*
 #* Copyright (C) 2010  Markus Koetter
-#* 
+#*
 #* This program is free software; you can redistribute it and/or
 #* modify it under the terms of the GNU General Public License
 #* as published by the Free Software Foundation; either version 2
 #* of the License, or (at your option) any later version.
-#* 
+#*
 #* This program is distributed in the hope that it will be useful,
 #* but WITHOUT ANY WARRANTY; without even the implied warranty of
 #* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #* GNU General Public License for more details.
-#* 
+#*
 #* You should have received a copy of the GNU General Public License
 #* along with this program; if not, write to the Free Software
 #* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-#* 
-#* 
-#*             contact nepenthesdev@gmail.com  
+#*
+#*
+#*             contact nepenthesdev@gmail.com
 #*
 #*******************************************************************************/
 #*  This file was part of Scapy
@@ -106,67 +106,67 @@ class NegTokenInit(ASN1_Packet):
 #}
 #
 
-NegTokenTarg_negResults = { 
-	0: "completed",
+NegTokenTarg_negResults = {
+    0: "completed",
     1: "incomplete",
-	2: "reject"
+    2: "reject"
 }
 
 class ASN1_Class_NegTokenTargValue(ASN1_Class_UNIVERSAL):
-	name = "NegTokenTargValue"
-	negResult     = 0xa0
-	supportedMech = 0xa1
-	responseToken = 0xa2
-	mechListMIC   = 0xa3
+    name = "NegTokenTargValue"
+    negResult     = 0xa0
+    supportedMech = 0xa1
+    responseToken = 0xa2
+    mechListMIC   = 0xa3
 
 # negResult
 class ASN1_NegTokenTargValue_negResult(ASN1_SEQUENCE):
-	tag = ASN1_Class_NegTokenTargValue.negResult
+    tag = ASN1_Class_NegTokenTargValue.negResult
 
 class ASN1F_NegTokenTargValue_negResult(ASN1F_SEQUENCE):
-	ASN1_tag = ASN1_Class_NegTokenTargValue.negResult
+    ASN1_tag = ASN1_Class_NegTokenTargValue.negResult
 
 class BERcodec_NegTokenTargValue_negResult(BERcodec_SEQUENCE):
-	tag = ASN1_Class_NegTokenTargValue.negResult
+    tag = ASN1_Class_NegTokenTargValue.negResult
 
 # supportedMech
 class ASN1_NegTokenTargValue_supportedMech(ASN1_SEQUENCE):
-	tag = ASN1_Class_NegTokenTargValue.supportedMech
+    tag = ASN1_Class_NegTokenTargValue.supportedMech
 
 class ASN1F_NegTokenTargValue_supportedMech(ASN1F_SEQUENCE):
-	ASN1_tag = ASN1_Class_NegTokenTargValue.supportedMech
+    ASN1_tag = ASN1_Class_NegTokenTargValue.supportedMech
 
 class BERcodec_NegTokenTargValue_supportedMech(BERcodec_SEQUENCE):
-	tag = ASN1_Class_NegTokenTargValue.supportedMech
+    tag = ASN1_Class_NegTokenTargValue.supportedMech
 
 # responseToken
 class ASN1_NegTokenTargValue_responseToken(ASN1_SEQUENCE):
-	tag = ASN1_Class_NegTokenTargValue.responseToken
+    tag = ASN1_Class_NegTokenTargValue.responseToken
 
 class ASN1F_NegTokenTargValue_responseToken(ASN1F_SEQUENCE):
-	ASN1_tag = ASN1_Class_NegTokenTargValue.responseToken
+    ASN1_tag = ASN1_Class_NegTokenTargValue.responseToken
 
 class BERcodec_NegTokenTargValue_responseToken(BERcodec_SEQUENCE):
-	tag = ASN1_Class_NegTokenTargValue.responseToken
+    tag = ASN1_Class_NegTokenTargValue.responseToken
 
 # mechListMIC
 class ASN1_NegTokenTargValue_mechListMIC(ASN1_SEQUENCE):
-	tag = ASN1_Class_NegTokenTargValue.mechListMIC
+    tag = ASN1_Class_NegTokenTargValue.mechListMIC
 
 class ASN1F_NegTokenTargValue_mechListMIC(ASN1F_SEQUENCE):
-	ASN1_tag = ASN1_Class_NegTokenTargValue.mechListMIC
+    ASN1_tag = ASN1_Class_NegTokenTargValue.mechListMIC
 
 class BERcodec_NegTokenTargValue_mechListMIC(BERcodec_SEQUENCE):
-	tag = ASN1_Class_NegTokenTargValue.mechListMIC
+    tag = ASN1_Class_NegTokenTargValue.mechListMIC
 
 class NegTokenTarg(ASN1_Packet):
-	ASN1_codec = ASN1_Codecs.BER
-	ASN1_root = ASN1F_SEQUENCE(
-		ASN1F_optionnal(ASN1F_NegTokenTargValue_negResult(ASN1F_ENUMERATED("negResult",1,NegTokenTarg_negResults))),
-		ASN1F_optionnal(ASN1F_NegTokenTargValue_supportedMech(ASN1F_OID("supportedMech","1"))),
-		ASN1F_optionnal(ASN1F_NegTokenTargValue_responseToken(ASN1F_STRING("responseToken",None))),
-		ASN1F_optionnal(ASN1F_NegTokenTargValue_mechListMIC(ASN1F_STRING("mechListMIC",None)))
-	)
+    ASN1_codec = ASN1_Codecs.BER
+    ASN1_root = ASN1F_SEQUENCE(
+        ASN1F_optionnal(ASN1F_NegTokenTargValue_negResult(ASN1F_ENUMERATED("negResult",1,NegTokenTarg_negResults))),
+        ASN1F_optionnal(ASN1F_NegTokenTargValue_supportedMech(ASN1F_OID("supportedMech","1"))),
+        ASN1F_optionnal(ASN1F_NegTokenTargValue_responseToken(ASN1F_STRING("responseToken",None))),
+        ASN1F_optionnal(ASN1F_NegTokenTargValue_mechListMIC(ASN1F_STRING("mechListMIC",None)))
+    )
 
 
 class SPNEGO(ASN1_Packet):
