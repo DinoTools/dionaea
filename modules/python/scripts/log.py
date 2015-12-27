57a78e583a39343ded3385ad5c571c8b020c292f
PhiBo <phibo@dinotools.org>
2015-12-27 22:34:24 +0100

    python/log - edit with autopep8 - E1,E4,E5
    + delete white spaces at EOL
    + delete some other tabs

--- modules/python/scripts/log.py
+++ modules/python/scripts/log.py
@@ -1,27 +1,27 @@
-#********************************************************************************
+#*************************************************************************
 #*                               Dionaea
 #*                           - catches bugs -
 #*
 #*
 #*
 #* Copyright (C) 2009  Paul Baecher & Markus Koetter
-#* 
+#*
 #* This program is free software; you can redistribute it and/or
 #* modify it under the terms of the GNU General Public License
 #* as published by the Free Software Foundation; either version 2
 #* of the License, or (at your option) any later version.
-#* 
+#*
 #* This program is distributed in the hope that it will be useful,
 #* but WITHOUT ANY WARRANTY; without even the implied warranty of
 #* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #* GNU General Public License for more details.
-#* 
+#*
 #* You should have received a copy of the GNU General Public License
 #* along with this program; if not, write to the Free Software
 #* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-#* 
-#* 
-#*             contact nepenthesdev@gmail.com  
+#*
+#*
+#*             contact nepenthesdev@gmail.com
 #*
 #*******************************************************************************/
 
@@ -33,25 +33,24 @@ global handler
 global logger
 
 class DionaeaLogHandler(logging.Handler):
-	def __init__(self):
-		logging.Handler.__init__(self, logging.DEBUG)
-	def emit(self,record):
-		dlhfn(record.name, record.levelno, record.pathname, record.lineno, record.msg)
+    def __init__(self):
+        logging.Handler.__init__(self, logging.DEBUG)
+    def emit(self,record):
+        dlhfn(record.name, record.levelno,
+              record.pathname, record.lineno, record.msg)
 
 def start():
-	pass
+    pass
 
 def new():
-	global logger
-	global handler
-	logger = logging.getLogger('')
-	logger.setLevel(logging.DEBUG)
-	handler = DionaeaLogHandler()
-	logger.addHandler(handler)
+    global logger
+    global handler
+    logger = logging.getLogger('')
+    logger.setLevel(logging.DEBUG)
+    handler = DionaeaLogHandler()
+    logger.addHandler(handler)
 
 def stop():
-	global logger
-	global handler
-	logger.removeHandler(handler)
-
-
+    global logger
+    global handler
+    logger.removeHandler(handler)
