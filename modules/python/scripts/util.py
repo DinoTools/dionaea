62f9644aa90e9aa694ee3bb1c55ec000fa3b6fee
PhiBo <phibo@dinotools.org>
2015-12-27 22:38:40 +0100

    python/util - edit with autopep8 - E1,E4,E5
    + delete white spaces at EOL
    + delete some other tabs

--- modules/python/scripts/util.py
+++ modules/python/scripts/util.py
@@ -1,4 +1,4 @@
-#********************************************************************************
+#*************************************************************************
 #*                               Dionaea
 #*                           - catches bugs -
 #*
@@ -6,33 +6,33 @@
 #*
 #* Copyright (C) 2010  Mark Schloesser
 #* Copyright (C) 2009  Paul Baecher & Markus Koetter
-#* 
+#*
 #* This program is free software; you can redistribute it and/or
 #* modify it under the terms of the GNU General Public License
 #* as published by the Free Software Foundation; either version 2
 #* of the License, or (at your option) any later version.
-#* 
+#*
 #* This program is distributed in the hope that it will be useful,
 #* but WITHOUT ANY WARRANTY; without even the implied warranty of
 #* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #* GNU General Public License for more details.
-#* 
+#*
 #* You should have received a copy of the GNU General Public License
 #* along with this program; if not, write to the Free Software
 #* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-#* 
-#* 
-#*             contact nepenthesdev@gmail.com  
+#*
+#*
+#*             contact nepenthesdev@gmail.com
 #*
 #*******************************************************************************/
 
 import hashlib
 
 def md5file(filename):
-	return hashfile(filename, hashlib.md5())
+    return hashfile(filename, hashlib.md5())
 
 def sha512file(filename):
-	return hashfile(filename, hashlib.sha512())
+    return hashfile(filename, hashlib.sha512())
 
 def hashfile(filename, digest):
     fh = open(filename, mode="rb")
