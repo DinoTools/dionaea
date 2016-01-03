cad6da2dd5492de9d358fcb61e232779022919ce
PhiBo <phibo@dinotools.org>
2016-01-03 13:33:17 +0100

    python - Use auto register to load ihandler

--- modules/python/scripts/ihandlers.py
+++ modules/python/scripts/ihandlers.py
@@ -27,120 +27,44 @@
 #*******************************************************************************/
 
 import logging
+import pkgutil
+import traceback
+from dionaea import IHandlerLoader, load_submodules
 from dionaea.core import g_dionaea
 
 logger = logging.getLogger('ihandlers')
 logger.setLevel(logging.DEBUG)
 
-# reload service imports
-#imp.reload(dionaea.tftp)
-#imp.reload(dionaea.ftp)
-#imp.reload(dionaea.cmd)
-#imp.reload(dionaea.emu)
-#imp.reload(dionaea.store)
-
 # global handler list
 # keeps a ref on our handlers
 # allows restarting
-global g_handlers
+g_handlers = {}
+
+
+def new():
+    load_submodules()
+
 
 def start():
     logger.warn("START THE IHANDLERS")
-    for i in g_handlers:
-        method = getattr(i, "start", None)
-        if method != None:
-            method()
+    for h in IHandlerLoader:
+        if h.name not in g_dionaea.config()['modules']['python']['ihandlers']['handlers']:
+            continue
+        if h not in g_handlers:
+            g_handlers[h] = []
 
-def new():
-    global g_handlers
-    g_handlers = []
+        handlers = h.start()
+        if isinstance(handlers, (list, tuple)):
+            g_handlers[h] += handlers
+        else:
+            g_handlers[h].append(handlers)
 
-    if "ftpdownload" in g_dionaea.config()['modules']['python']['ihandlers']['handlers']:
-        import dionaea.ftp
-        g_handlers.append(
-            dionaea.ftp.ftpdownloadhandler('dionaea.download.offer'))
-
-    if "tftpdownload" in g_dionaea.config()['modules']['python']['ihandlers']['handlers']:
-        import dionaea.tftp
-        g_handlers.append(
-            dionaea.tftp.tftpdownloadhandler('dionaea.download.offer'))
-
-    if "emuprofile" in g_dionaea.config()['modules']['python']['ihandlers']['handlers']:
-        import dionaea.emu
-        g_handlers.append(
-            dionaea.emu.emuprofilehandler('dionaea.module.emu.profile'))
-
-    if "cmdshell" in g_dionaea.config()['modules']['python']['ihandlers']['handlers']:
-        import dionaea.cmd
-        g_handlers.append(
-            dionaea.cmd.cmdshellhandler('dionaea.service.shell.*'))
-
-    if "store" in g_dionaea.config()['modules']['python']['ihandlers']['handlers']:
-        import dionaea.store
-        g_handlers.append(
-            dionaea.store.storehandler('dionaea.download.complete'))
-
-    if "uniquedownload" in g_dionaea.config()['modules']['python']['ihandlers']['handlers']:
-        import dionaea.test
-        g_handlers.append(
-            dionaea.test.uniquedownloadihandler('dionaea.download.complete.unique'))
-
-    if "surfids" in g_dionaea.config()['modules']['python']['ihandlers']['handlers']:
-        import dionaea.surfids
-        g_handlers.append(dionaea.surfids.surfidshandler('*'))
-
-    if "logsql" in g_dionaea.config()['modules']['python']['ihandlers']['handlers']:
-        import dionaea.logsql
-        g_handlers.append(dionaea.logsql.logsqlhandler("*"))
-
-    if "p0f" in g_dionaea.config()['modules']['python']['ihandlers']['handlers']:
-        import dionaea.p0f
-        g_handlers.append(
-            dionaea.p0f.p0fhandler(g_dionaea.config()['modules']['python']['p0f']['path']))
-
-    if "logxmpp" in g_dionaea.config()['modules']['python']['ihandlers']['handlers']:
-        import dionaea.logxmpp
-        from random import choice
-        import string
-        for client in g_dionaea.config()['modules']['python']['logxmpp']:
-            conf = g_dionaea.config()['modules']['python']['logxmpp'][client]
-            if 'resource' in conf:
-                resource = conf['resource']
-            else:
-                resource = ''.join(
-                    [choice(string.ascii_letters) for i in range(8)])
-            print("client %s \n\tserver %s:%s username %s password %s resource %s muc %s\n\t%s" % (client, conf[
-                  'server'], conf['port'], conf['username'], conf['password'], resource, conf['muc'], conf['config']))
-            x = dionaea.logxmpp.logxmpp(conf['server'], int(conf['port']), conf[
-                                        'username'], conf['password'], resource, conf['muc'], conf['config'])
-            g_handlers.append(x)
-
-    if "nfq" in g_dionaea.config()['modules']['python']['ihandlers']['handlers']:
-        import dionaea.nfq
-        g_handlers.append(dionaea.nfq.nfqhandler())
-
-
-    if "virustotal" in g_dionaea.config()['modules']['python']['ihandlers']['handlers']:
-        import dionaea.virustotal
-        g_handlers.append(dionaea.virustotal.virustotalhandler('*'))
-
-    if "mwserv" in g_dionaea.config()['modules']['python']['ihandlers']['handlers']:
-        import dionaea.mwserv
-        g_handlers.append(dionaea.mwserv.mwservhandler('*'))
-
-    if "submit_http" in g_dionaea.config()['modules']['python']['ihandlers']['handlers']:
-        import dionaea.submit_http
-        g_handlers.append(dionaea.submit_http.handler('*'))
-
-    if "fail2ban" in g_dionaea.config()['modules']['python']['ihandlers']['handlers']:
-        import dionaea.fail2ban
-        g_handlers.append(dionaea.fail2ban.fail2banhandler())
 
 def stop():
-    global g_handlers
-    for i in g_handlers:
-        logger.debug("deleting %s" % str(i))
-        i.stop()
-        del i
+    for handler_loader, ihandlers in g_handlers:
+        for i in ihandlers:
+            logger.debug("deleting %s" % str(i))
+            handler_loader.stop(i)
+            del i
     del g_handlers
 
