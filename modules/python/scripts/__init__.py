16e75cdc5c16f70ac8694d656bd061c0fa984626
PhiBo <phibo@dinotools.org>
2016-01-03 13:31:23 +0100

    python - Add new auto register class

---
+++ modules/python/scripts/__init__.py
@@ -0,0 +1,62 @@
+import logging
+import pkgutil
+import traceback
+
+logger = logging.getLogger('dionaea')
+logger.setLevel(logging.DEBUG)
+
+loaded_submodules = []
+
+
+class RegisterClasses(type):
+    def __init__(self, name, bases, nmspc):
+        super(RegisterClasses, self).__init__(name, bases, nmspc)
+        if not hasattr(self, 'registry'):
+            self.registry = set()
+
+        self.registry.add(self)
+        self.registry -= set(bases)
+
+    def __iter__(self):
+        return iter(self.registry)
+
+
+class ServiceLoader(object, metaclass=RegisterClasses):
+    @classmethod
+    def start(cls, addr, iface=None):
+        raise NotImplementedError("do it")
+
+    @classmethod
+    def stop(cls, daemon):
+        daemon.close()
+
+
+class IHandlerLoader(object, metaclass=RegisterClasses):
+    @classmethod
+    def start(cls):
+        raise NotImplementedError("do it")
+
+    @classmethod
+    def stop(cls, ihandler):
+        ihandler.stop()
+
+
+def load_submodules(base_pkg=None):
+    if base_pkg is None:
+        import dionaea as base_pkg
+
+    prefix = base_pkg.__name__ + "."
+    for importer, modname, ispkg in pkgutil.iter_modules(base_pkg.__path__, prefix):
+        if modname in loaded_submodules:
+            continue
+
+        logger.info("Import module {}".format(modname))
+        try:
+            __import__(modname, fromlist="dummy")
+        except Exception as e:
+            logger.warning("Error loading module: {}".format(str(e)))
+
+            for msg in traceback.format_exc().split("\n"):
+                logger.warning(msg.rstrip())
+
+        loaded_submodules.append(modname)
