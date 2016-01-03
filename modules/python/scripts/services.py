039e484844215251ea38469b2babdaacb1eb5421
PhiBo <phibo@dinotools.org>
2016-01-03 13:32:46 +0100

    python - Use auto register to load services

--- modules/python/scripts/services.py
+++ modules/python/scripts/services.py
@@ -30,32 +30,17 @@ import logging
 import fnmatch
 
 from dionaea.core import g_dionaea, ihandler
+from dionaea import ServiceLoader, load_submodules
 
-# service imports
-import dionaea.http
-import dionaea.tftp
-import dionaea.ftp
-import dionaea.mirror
-from dionaea.smb import smb
-import dionaea.sip
-from dionaea.mssql import mssql
-from dionaea.mysql import mysql
-from dionaea.pptp import pptp
-from dionaea.mqtt import mqtt
-from dionaea.upnp import upnp
 
 logger = logging.getLogger('services')
-
-# reload service imports
-#imp.reload(dionaea.http)
-#imp.reload(dionaea.tftp)
-#imp.reload(dionaea.smb)
 
 # global slave
 # keeps track of running services (daemons)
 # able to restart them
 global g_slave
 global addrs
+
 
 class slave():
     def __init__(self):
@@ -70,15 +55,21 @@ class slave():
                 for addr in addrs[iface]:
                     print(addr)
                     self.daemons[addr] = {}
-                    for service in self.services:
-                        print(service)
-                        if not service in self.daemons[addr]:
+                    for service in ServiceLoader:
+                        if service.name not in g_dionaea.config()['modules']['python']['services']['serve']:
+                            continue
+                        if service not in self.daemons[addr]:
                             self.daemons[addr][service] = []
-                        daemon = service.start(service, addr, iface=iface)
-                        self.daemons[addr][service].append(daemon)
+                        print(service)
+                        daemons = service.start(addr, iface=iface)
+                        if isinstance(daemons, (list, tuple)):
+                            self.daemons[addr][service] += daemons
+                        else:
+                            self.daemons[addr][service].append(daemons)
         except Exception as e:
             raise e
         print(self.daemons)
+
 
 # for netlink,
 # allows listening on new addrs
@@ -111,159 +102,6 @@ class nlslave(ihandler):
     def start(self, addrs):
         pass
 
-class service:
-    def start(self, addr, iface=None):
-        raise NotImplementedError("do it")
-    def stop(self, daemon):
-        raise NotImplementedError("do it")
-
-class httpservice(service):
-    def start(self, addr, iface=None):
-        daemon = dionaea.http.httpd(proto='tcp')
-        daemon.bind(addr, 80, iface=iface)
-        daemon.chroot(g_dionaea.config()['modules']['python']['http']['root'])
-        daemon.listen()
-        return daemon
-    def stop(self, daemon):
-        daemon.close()
-
-class httpsservice(service):
-    def start(self, addr, iface=None):
-        daemon = dionaea.http.httpd(proto='tls')
-        daemon.bind(addr, 443, iface=iface)
-        daemon.chroot(g_dionaea.config()['modules']['python']['http']['root'])
-        daemon.listen()
-        return daemon
-    def stop(self, daemon):
-        daemon.close()
-
-
-class ftpservice(service):
-    def start(self, addr,  iface=None):
-        daemon = dionaea.ftp.ftpd()
-        daemon.chroot(g_dionaea.config()['modules']['python']['ftp']['root'])
-        daemon.bind(addr, 21, iface=iface)
-        daemon.listen()
-        return daemon
-    def stop(self, daemon):
-        daemon.close()
-
-
-class tftpservice(service):
-    def start(self, addr,  iface=None):
-        daemon = dionaea.tftp.TftpServer()
-        daemon.chroot(g_dionaea.config()['modules']['python']['tftp']['root'])
-        daemon.bind(addr, 69, iface=iface)
-        return daemon
-    def stop(self, daemon):
-        daemon.close()
-
-class mirrorservice(service):
-    def start(self, addr, iface=None):
-        daemon = dionaea.mirror.mirrord('tcp', addr, 42, iface)
-        return daemon
-    def stop(self, daemon):
-        daemon.close()
-
-class smbservice(service):
-    def start(self, addr,  iface=None):
-        daemon = smb.smbd()
-        daemon.bind(addr, 445, iface=iface)
-        daemon.listen()
-        return daemon
-    def stop(self, daemon):
-        daemon.close()
-
-class epmapservice(service):
-    def start(self, addr,  iface=None):
-        daemon = smb.epmapper()
-        daemon.bind(addr, 135, iface=iface)
-        daemon.listen()
-        return daemon
-    def stop(self, daemon):
-        daemon.close()
-
-class siptcpservice(service):
-    def start(self, addr, iface=None):
-        port = int(
-            g_dionaea.config()['modules']['python']['sip']['tcp'].get('port', 5060))
-        daemon = dionaea.sip.SipSession(proto = 'tcp')
-        daemon.bind(addr, port, iface=iface)
-        daemon.listen()
-        return daemon
-
-    def stop(self, daemon):
-        daemon.close()
-
-class siptlsservice(service):
-    def start(self, addr, iface=None):
-        port = int(
-            g_dionaea.config()['modules']['python']['sip']['tls'].get('port', 5061))
-        daemon = dionaea.sip.SipSession(proto = 'tls')
-        daemon.bind(addr, port, iface=iface)
-        daemon.listen()
-        return daemon
-
-    def stop(self, daemon):
-        daemon.close()
-
-class sipudpservice(service):
-    def start(self, addr, iface=None):
-        port = int(
-            g_dionaea.config()['modules']['python']['sip']['udp'].get('port', 5060))
-        daemon = dionaea.sip.SipSession(proto = 'udp')
-        daemon.bind(addr, port, iface=iface)
-        daemon.listen()
-        return daemon
-
-    def stop(self, daemon):
-        daemon.close()
-
-class mssqlservice(service):
-    def start(self, addr,  iface=None):
-        daemon = mssql.mssqld()
-        daemon.bind(addr, 1433, iface=iface)
-        daemon.listen()
-        return daemon
-    def stop(self, daemon):
-        daemon.close()
-
-class mysqlservice(service):
-    def start(self, addr,  iface=None):
-        daemon = mysql.mysqld()
-        daemon.bind(addr, 3306, iface=iface)
-        daemon.listen()
-        return daemon
-    def stop(self, daemon):
-        daemon.close()
-
-class pptpservice(service):
-    def start(self, addr,  iface=None):
-        daemon = pptp.pptpd()
-        daemon.bind(addr, 1723, iface=iface)
-        daemon.listen()
-        return daemon
-    def stop(self, daemon):
-        daemon.close()
-
-class mqttservice(service):
-    def start(self, addr,  iface=None):
-        daemon = mqtt.mqttd()
-        daemon.bind(addr, 1883, iface=iface)
-        daemon.listen()
-        return daemon
-    def stop(self, daemon):
-        daemon.close()
-
-class upnpservice(service):
-    def start(self, addr,  iface=None):
-        daemon = upnp.upnpd()
-        daemon.bind(addr, 1900, iface=iface)
-        daemon.chroot(g_dionaea.config()['modules']['python']['upnp']['root'])
-        daemon.listen()
-        return daemon
-    def stop(self, daemon):
-        daemon.close()
 
 #mode = 'getifaddrs'
 #mode = 'manual'
@@ -302,47 +140,10 @@ def new():
         g_slave = nlslave()
         g_slave.ifaces = g_dionaea.config()['listen']['interfaces']
 
-    if "http" in g_dionaea.config()['modules']['python']['services']['serve']:
-        g_slave.services.append(httpservice)
+    load_submodules()
 
-    if "https" in g_dionaea.config()['modules']['python']['services']['serve']:
-        g_slave.services.append(httpsservice)
 
-    if "tftp" in g_dionaea.config()['modules']['python']['services']['serve']:
-        g_slave.services.append(tftpservice)
-
-    if "ftp" in g_dionaea.config()['modules']['python']['services']['serve']:
-        g_slave.services.append(ftpservice)
-
-    if "mirror" in g_dionaea.config()['modules']['python']['services']['serve']:
-        g_slave.services.append(mirrorservice)
-
-    if "smb" in g_dionaea.config()['modules']['python']['services']['serve']:
-        g_slave.services.append(smbservice)
-
-    if "epmap" in g_dionaea.config()['modules']['python']['services']['serve']:
-        g_slave.services.append(epmapservice)
-
-    if "sip" in g_dionaea.config()['modules']['python']['services']['serve']:
-        for proto,factory in {'tcp': siptcpservice, 'tls': siptlsservice, 'udp': sipudpservice}.items():
-            if proto in g_dionaea.config()['modules']['python']['sip']:
-                g_slave.services.append(factory)
-
-    if "mssql" in g_dionaea.config()['modules']['python']['services']['serve']:
-        g_slave.services.append(mssqlservice)
-
-    if "mysql" in g_dionaea.config()['modules']['python']['services']['serve']:
-        g_slave.services.append(mysqlservice)
-
-    if "pptp" in g_dionaea.config()['modules']['python']['services']['serve']:
-        g_slave.services.append(pptpservice)
-
-    if "mqtt" in g_dionaea.config()['modules']['python']['services']['serve']:
-        g_slave.services.append(mqttservice)
-
-    if "upnp" in g_dionaea.config()['modules']['python']['services']['serve']:
-        g_slave.services.append(upnpservice)
-
+def start():
     g_slave.start(addrs)
 
 
