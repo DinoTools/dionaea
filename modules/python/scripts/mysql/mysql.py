7ade72e7a6eefcb716580b27cad6809f061ac7a5
PhiBo <phibo@dinotools.org>
2016-01-25 22:08:13 +0100

    python/mysql - Fix code style

--- modules/python/scripts/mysql/mysql.py
+++ modules/python/scripts/mysql/mysql.py
@@ -36,12 +36,13 @@ logger = logging.getLogger('mysqld')
 
 
 class mysqld(connection):
-    def __init__ (self):
-        connection.__init__(self,"tcp")
+    def __init__(self):
+        connection.__init__(self, "tcp")
+        self.config = None
+        self.state = ""
 
     def handle_established(self):
-        self.config = g_dionaea.config()['modules']['python'][
-            'mysql']['databases']
+        self.config = g_dionaea.config()['modules']['python']['mysql']['databases']
 
         self.state = 'greeting'
         a = MySQL_Packet_Header(Number=0) / MySQL_Server_Greeting()
@@ -74,22 +75,23 @@ class mysqld(connection):
         # FIXME sqlite does not allow ? for PRAGMA? I'm not afraid of SQLi here
         # though.
         result = self.cursor.execute(query)
-        names = [result.description[x][0]
-                 for x in range(len(result.description))]
+        names = [result.description[x][0] for x in range(len(result.description))]
         result = [dict(zip(names, i)) for i in result]
         for res in result:
-            x = MySQL_Result_Field(Catalog='def',
-                                   Database=self.database,
-                                   Table=p.Table[:-1],
-                                   ORGTable=p.Table[:-1],
-                                   Name=res['name'].encode('ascii'),
-                                   ORGName=res['name'].encode('ascii'),
-                                   CharSet=33,
-                                   Length=20,
-                                   Type=FIELD_TYPE_VAR_STRING,
-                                   Flags=0, #0x4203,
-                                   Decimals=0,
-                                   Default='0')
+            x = MySQL_Result_Field(
+                Catalog='def',
+                Database=self.database,
+                Table=p.Table[:-1],
+                ORGTable=p.Table[:-1],
+                Name=res['name'].encode('ascii'),
+                ORGName=res['name'].encode('ascii'),
+                CharSet=33,
+                Length=20,
+                Type=FIELD_TYPE_VAR_STRING,
+                Flags=0,  # 0x4203,
+                Decimals=0,
+                Default='0'
+            )
             r.append(x)
         r.append(MySQL_Result_EOF(ServerStatus=0x002))
         return r
@@ -99,68 +101,79 @@ class mysqld(connection):
         if re.match(b'SET ', p.Query, re.I):
             r = MySQL_Result_OK(Message="#2")
         elif re.match(b'select @@version_comment limit 1$', p.Query, re.I) or re.match(b'select version\(\)$', p.Query, re.I):
-            r = [MySQL_Result_Header(FieldCount=1),
-                 MySQL_Result_Field(Catalog='def',
-                                    Name='@@version_comment',
-                                    CharSet=33,
-                                    Length=75,
-                                    Type=FIELD_TYPE_VAR_STRING,
-                                    Flags=FLAG_NOT_NULL,
-                                    Decimals=0),
-                 MySQL_Result_EOF(ServerStatus=0x002),
-                 MySQL_Result_Row_Data(
-                     ColumnValues=['Gentoo Linux mysql-5.0.54\0']),
-                 MySQL_Result_EOF(ServerStatus=0x002)]
-
+            r = [
+                MySQL_Result_Header(FieldCount=1),
+                MySQL_Result_Field(
+                    Catalog='def',
+                    Name='@@version_comment',
+                    CharSet=33,
+                    Length=75,
+                    Type=FIELD_TYPE_VAR_STRING,
+                    Flags=FLAG_NOT_NULL,
+                    Decimals=0
+                ),
+                MySQL_Result_EOF(ServerStatus=0x002),
+                MySQL_Result_Row_Data(ColumnValues=['Gentoo Linux mysql-5.0.54\0']),
+                MySQL_Result_EOF(ServerStatus=0x002)
+            ]
         elif p.Query == re.match(b'SELECT DATABASE\(\)$', p.Query, re.I):
-            r = [MySQL_Result_Header(FieldCount=1),
-                 MySQL_Result_Field(Catalog='def',
-                                    Name=b'DATABASE()',
-                                    CharSet=33,
-                                    Length=75,
-                                    Type=FIELD_TYPE_VAR_STRING,
-                                    Flags=FLAG_NOT_NULL,
-                                    Decimals=0),
-                 MySQL_Result_EOF(ServerStatus=0x002),
-                 MySQL_Result_Row_Data(ColumnValues=[self.database]),
-                 MySQL_Result_EOF(ServerStatus=0x002)]
+            r = [
+                MySQL_Result_Header(FieldCount=1),
+                MySQL_Result_Field(
+                    Catalog='def',
+                    Name=b'DATABASE()',
+                    CharSet=33,
+                    Length=75,
+                    Type=FIELD_TYPE_VAR_STRING,
+                    Flags=FLAG_NOT_NULL,
+                    Decimals=0
+                ),
+                MySQL_Result_EOF(ServerStatus=0x002),
+                MySQL_Result_Row_Data(ColumnValues=[self.database]),
+                MySQL_Result_EOF(ServerStatus=0x002)
+            ]
         elif p.Query == re.match(b'show databases$', p.Query, re.I):
-            r = [MySQL_Result_Header(FieldCount=1),
-                 MySQL_Result_Field(Catalog='def',
-                                    Table=b'SCHEMATA',
-                                    Name=b'Database',
-                                    CharSet=33,
-                                    Length=192,
-                                    Type=FIELD_TYPE_VAR_STRING,
-                                    Flags=FLAG_NOT_NULL,
-                                    Decimals=0),
-                 MySQL_Result_EOF(ServerStatus=0x002)]
+            r = [
+                MySQL_Result_Header(FieldCount=1),
+                MySQL_Result_Field(
+                    Catalog='def',
+                    Table=b'SCHEMATA',
+                    Name=b'Database',
+                    CharSet=33,
+                    Length=192,
+                    Type=FIELD_TYPE_VAR_STRING,
+                    Flags=FLAG_NOT_NULL,
+                    Decimals=0
+                ),
+                MySQL_Result_EOF(ServerStatus=0x002)
+            ]
 
             for i in self.config.keys():
                 r.append(MySQL_Result_Row_Data(ColumnValues=[i]))
 
-#			r.append(MySQL_Result_Row_Data(ColumnValues=['information_schema']))
+            # r.append(MySQL_Result_Row_Data(ColumnValues=['information_schema']))
             r.append(MySQL_Result_EOF(ServerStatus=0x002))
         elif p.Query == re.match(b'show tables$', p.Query, re.I):
-            r = [MySQL_Result_Header(FieldCount=1),
-                 MySQL_Result_Field(Catalog='def',
-                                    Table=b'TABLE_NAMES',
-                                    Name=b'Tables_in_test',
-                                    CharSet=33,
-                                    Length=192,
-                                    Type=FIELD_TYPE_VAR_STRING,
-                                    Flags=FLAG_NOT_NULL,
-                                    Decimals=0),
-                 MySQL_Result_EOF(ServerStatus=0x002)]
+            r = [
+                MySQL_Result_Header(FieldCount=1),
+                MySQL_Result_Field(
+                    Catalog='def',
+                    Table=b'TABLE_NAMES',
+                    Name=b'Tables_in_test',
+                    CharSet=33,
+                    Length=192,
+                    Type=FIELD_TYPE_VAR_STRING,
+                    Flags=FLAG_NOT_NULL,
+                    Decimals=0
+                ),
+                MySQL_Result_EOF(ServerStatus=0x002)
+            ]
 
-            result = self.cursor.execute(
-                "select tbl_name from sqlite_master where type = 'table'")
-            names = [result.description[x][0]
-                     for x in range(len(result.description))]
+            result = self.cursor.execute("select tbl_name from sqlite_master where type = 'table'")
+            names = [result.description[x][0] for x in range(len(result.description))]
             result = [dict(zip(names, i)) for i in result]
             for res in result:
-                x = MySQL_Result_Row_Data(
-                    ColumnValues=[res[name] for name in names])
+                x = MySQL_Result_Row_Data(ColumnValues=[res[name] for name in names])
                 r.append(x)
             r.append(MySQL_Result_EOF(ServerStatus=0x002))
         else:
@@ -173,27 +186,29 @@ class mysqld(connection):
                 if result.description is None:
                     r = MySQL_Result_OK()
                 else:
-                    names = [result.description[x][0]
-                             for x in range(len(result.description))]
+                    names = [result.description[x][0] for x in range(len(result.description))]
                     print(result)
                     result = [dict(zip(names, i)) for i in result]
                     r = [MySQL_Result_Header(FieldCount=len(names))]
                     for name in names:
-                        r.append(MySQL_Result_Field(#Catalog='def',
-                            Table=b'',
-                            ORGTable=b"",
-                            Database=b"",
-                            Name=name,
-                            CharSet=33,
-                            Length=255,
-                            Type=FIELD_TYPE_VAR_STRING,
-                            Flags=FLAG_NOT_NULL,
-                            Decimals=0))
+                        r.append(
+                            MySQL_Result_Field(
+                                #Catalog='def',
+                                Table=b'',
+                                ORGTable=b"",
+                                Database=b"",
+                                Name=name,
+                                CharSet=33,
+                                Length=255,
+                                Type=FIELD_TYPE_VAR_STRING,
+                                Flags=FLAG_NOT_NULL,
+                                Decimals=0
+                            )
+                        )
                     r.append(MySQL_Result_EOF(ServerStatus=0x002))
                     for res in result:
-                        x = MySQL_Result_Row_Data(
-                            ColumnValues=[res[name] for name in names])
-#						x.show()
+                        x = MySQL_Result_Row_Data(ColumnValues=[res[name] for name in names])
+                        # x.show()
                         r.append(x),
                     r.append(MySQL_Result_EOF(ServerStatus=0x002))
             except Exception as e:
@@ -207,20 +222,18 @@ class mysqld(connection):
         while len(data) - offset >= 4:
             h = MySQL_Packet_Header(data[offset:offset+4])
             r = p = None
-            if len(data)-offset < h.Length+4:
+            if len(data)-offset < h.Length + 4:
                 break
 
             if self.state == 'greeting':
                 self.state = 'online'
-                p = MySQL_Client_Authentication(
-                    data[offset+4:offset+4+h.Length])
+                p = MySQL_Client_Authentication(data[offset+4:offset+4+h.Length])
                 if p.DatabaseName != b'\x00':
                     Database = p.DatabaseName[:-1]
                     if type(Database) == str:
                         Database = Database.encode('ascii')
                     if self._open_db(Database) == True:
-                        r = MySQL_Result_Error(
-                            Message="Could not open Database %s" % Database)
+                        r = MySQL_Result_Error(Message="Could not open Database %s" % Database)
                     else:
                         r = MySQL_Result_OK()
                 else:
