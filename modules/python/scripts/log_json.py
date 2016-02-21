0473e71f3e13c843c0146d004d3e17f46b2ad8d8
PhiBo <phibo@dinotools.org>
2016-02-21 22:27:58 +0100

    python/log_json - Add ihandler to submit json
    * Refs: #3
    * ihandler can submit information about connections as json formated
      data to http services

---
+++ modules/python/scripts/log_json.py
@@ -0,0 +1,167 @@
+#################################################################################
+#                                Dionaea
+#                            - catches bugs -
+#
+#  Copyright (c) 2016 PhiBo (DinoTools)
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+################################################################################
+
+import json
+import logging
+from urllib.parse import urlparse
+
+from dionaea import IHandlerLoader
+from dionaea.core import ihandler
+
+logger = logging.getLogger("log_json")
+logger.setLevel(logging.DEBUG)
+
+
+class HTTPHandler(object):
+    handle_schemes = ["http", "https"]
+
+    def __init__(self, url):
+        self.url = url
+
+    def submit(self, data):
+        from urllib.request import Request, urlopen
+        data = json.dumps(data)
+        data = data.encode("ASCII")
+        req = Request(
+            self.url,
+            data=data,
+            headers={
+                "Content-Type": "application/json"
+            }
+        )
+        # ToDo: parse response
+        response = urlopen(req)
+        # Debug:
+        #from pprint import pprint
+        #pprint(response)
+
+
+class LogJsonHandlerLoader(IHandlerLoader):
+    name = "log_json"
+
+    @classmethod
+    def start(cls):
+        from dionaea.core import g_dionaea
+        conf_mod_py = g_dionaea.config()["modules"]["python"]
+        config = conf_mod_py.get("log_json")
+        handlers = [LogJsonHandler("*", config=config)]
+        return handlers
+
+
+class LogJsonHandler(ihandler):
+    def __init__(self, path, config=None):
+        logger.debug("%s ready!", self.__class__.__name__)
+        ihandler.__init__(self, path)
+        self.path = path
+        self._config = config
+        self.db_session = None
+
+        self.attacks = {}
+        self.handlers = []
+        handlers = config.get("handlers")
+        if not isinstance(handlers, list) or len(handlers) == 0:
+            logger.warning("No handlers specified")
+            # Set empty list on error
+            handlers = []
+
+        for handler in handlers:
+            url = urlparse(handler)
+            for h in (HTTPHandler,):
+                if url.scheme in h.handle_schemes:
+                    self.handlers.append(h(url=handler))
+                    break
+
+    def handle_incident(self, icd):
+        #        print("unknown")
+        pass
+
+    def _serialize_connection(self, icd, connection_type):
+        con = icd.con
+
+        data = {
+            "connection": {
+                "local": {
+                    "address": con.local.host,
+                    "port": con.local.port
+                },
+                "protocol": con.protocol,
+                "remote": {
+                    "address": con.remote.host,
+                    "port": con.remote.port,
+                    "hostname": con.remote.hostname
+                },
+                "transport": con.transport,
+                "type": connection_type
+            }
+        }
+        self.attacks[con] = data
+
+    def handle_incident_dionaea_connection_tcp_listen(self, icd):
+        self._serialize_connection(icd, "listen")
+        con = icd.con
+        logger.info("listen connection on %s:%i" % (con.remote.host, con.remote.port))
+
+    def handle_incident_dionaea_connection_tls_listen(self, icd):
+        self._serialize_connection(icd, "listen")
+        con = icd.con
+        logger.info("listen connection on %s:%i" % (con.remote.host, con.remote.port))
+
+    def handle_incident_dionaea_connection_tcp_connect(self, icd):
+        self._serialize_connection(icd, "connect")
+        con = icd.con
+        logger.info("connect connection to %s/%s:%i from %s:%i" % (con.remote.host, con.remote.hostname, con.remote.port, con.local.host, con.local.port))
+
+    def handle_incident_dionaea_connection_tls_connect(self, icd):
+        self._serialize_connection(icd, "connect")
+        con = icd.con
+        logger.info("connect connection to %s/%s:%i from %s:%i" % (con.remote.host, con.remote.hostname, con.remote.port, con.local.host, con.local.port))
+
+    def handle_incident_dionaea_connection_udp_connect(self, icd):
+        self._serialize_connection(icd, "connect")
+        con = icd.con
+        logger.info("connect connection to %s/%s:%i from %s:%i" % (con.remote.host, con.remote.hostname, con.remote.port, con.local.host, con.local.port))
+
+    def handle_incident_dionaea_connection_tcp_accept(self, icd):
+        self._serialize_connection(icd, "accept")
+        con = icd.con
+        logger.info("accepted connection from %s:%i to %s:%i" % (con.remote.host, con.remote.port, con.local.host, con.local.port))
+
+    def handle_incident_dionaea_connection_tls_accept(self, icd):
+        self._serialize_connection(icd, "accept")
+        con = icd.con
+        logger.info("accepted connection from %s:%i to %s:%i" % (con.remote.host, con.remote.port, con.local.host, con.local.port))
+
+    def handle_incident_dionaea_connection_tcp_reject(self, icd):
+        self._serialize_connection(icd, "reject")
+        con = icd.con
+        logger.info("reject connection from %s:%i to %s:%i" % (con.remote.host, con.remote.port, con.local.host, con.local.port))
+
+    def handle_incident_dionaea_connection_free(self, icd):
+        con = icd.con
+        if con in self.attacks:
+            data = self.attacks.get(con)
+            if data:
+                for handler in self.handlers:
+                    handler.submit(data)
+            del self.attacks[con]
+        else:
+            logger.warn("no attack data for %s:%s" % (con.local.host, con.local.port))
