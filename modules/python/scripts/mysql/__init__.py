7c36c37f89fc6d8d4963ecda9313b7542fcd6d2d
PhiBo <phibo@dinotools.org>
2016-04-18 00:11:19 +0200

    python/mysql - Load config from dict

--- modules/python/scripts/mysql/__init__.py
+++ modules/python/scripts/mysql/__init__.py
@@ -6,8 +6,9 @@ class MYSQLService(ServiceLoader):
     name = "mysql"
 
     @classmethod
-    def start(cls, addr,  iface=None):
+    def start(cls, addr,  iface=None, config=None):
         daemon = mysqld()
+        daemon.apply_config(config)
         daemon.bind(addr, 3306, iface=iface)
         daemon.listen()
         return daemon
