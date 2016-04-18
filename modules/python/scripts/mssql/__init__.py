3ec04aa88e8803a29a3f31f1c8f5244b7b682184
PhiBo <phibo@dinotools.org>
2016-04-18 23:44:38 +0200

    python/mssql - Make service compatible

--- modules/python/scripts/mssql/__init__.py
+++ modules/python/scripts/mssql/__init__.py
@@ -5,8 +5,9 @@ class MSSQLService(ServiceLoader):
     name = "mssql"
 
     @classmethod
-    def start(cls, addr,  iface=None):
+    def start(cls, addr,  iface=None, config=config):
         daemon = mssqld()
+        daemon.apply_config(config)
         daemon.bind(addr, 1433, iface=iface)
         daemon.listen()
         return daemon
