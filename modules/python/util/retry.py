479b3a38a8786d452187ff05c592fab8bcaf05cc
PhiBo <phibo@dinotools.org>
2016-05-18 10:42:02 +0200

    util - Clean code

--- modules/python/util/retry.py
+++ modules/python/util/retry.py
@@ -9,19 +9,54 @@ import time
 
 parser = OptionParser()
 parser.add_option(
-    "-f", "--file", action="store", type="string", dest="filename")
-parser.add_option("-H", "--host", action="store", type="string", dest="host")
-parser.add_option("-p", "--port", action="store", type="int", dest="port")
+    "-f", "--file",
+    action="store",
+    type="string",
+    dest="filename"
+)
 parser.add_option(
-    "-s", "--send", action="store_true", dest="send", default=False)
+    "-H", "--host",
+    action="store",
+    type="string",
+    dest="host"
+)
 parser.add_option(
-    "-r", "--recv", action="store_true", dest="recv", default=False)
-parser.add_option("-t", "--tempfile", action="store",
-                  type="string", dest="tempfile", default="retrystream")
+    "-p", "--port",
+    action="store",
+    type="int",
+    dest="port"
+)
 parser.add_option(
-    "-u", "--udp", action="store_true", dest="udp", default=False)
+    "-s", "--send",
+    action="store_true",
+    dest="send",
+    default=False
+)
 parser.add_option(
-    "-v", "--verbose", action="store_true", dest="verbose", default=False)
+    "-r", "--recv",
+    action="store_true",
+    dest="recv",
+    default=False
+)
+parser.add_option(
+    "-t", "--tempfile",
+    action="store",
+    type="string",
+    dest="tempfile",
+    default="retrystream"
+)
+parser.add_option(
+    "-u", "--udp",
+    action="store_true",
+    dest="udp",
+    default=False
+)
+parser.add_option(
+    "-v", "--verbose",
+    action="store_true",
+    dest="verbose",
+    default=False
+)
 (options, args) = parser.parse_args()
 
 if os.path.exists(options.tempfile):
@@ -34,23 +69,23 @@ exec(import_string)
 
 print("doing " + options.filename)
 if options.send:
-    if options.udp == False:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    else:
+    if options.udp:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    else:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     s.connect((options.host, options.port))
 
 for i in stream:
     if i[0] == 'in':
         r = 0
-        if options.send == True:
+        if options.send:
             r = s.send(i[1])
         if options.verbose:
             print('send %i of %i bytes' % (r, len(i[1])))
     if i[0] == 'out':
         x = ""
-        if options.recv == True:
+        if options.recv:
             x = s.recv(len(i[1]))
         if options.verbose:
             print('recv %i of %i bytes' % ( len(x), len(i[1])) )
