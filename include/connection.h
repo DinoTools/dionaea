cd1726c08113fe83a42bc6303d3a7a366c8d4be9
PhiBo <phibo@dinotools.org>
2016-08-28 13:10:33 +0200

    dionaea - Param to report connection.free incident
    * If a connection fails before it has been fully accepted, reporting
      the incident might fail and is useless
    * Fixes #66

--- include/connection.h
+++ include/connection.h
@@ -269,7 +269,7 @@ void connection_process(struct connection *con);
 
 struct connection *connection_new(enum connection_transport type);
 void connection_free(struct connection *con);
-void connection_free_cb(struct ev_loop *loop, struct ev_timer *w, int revents);
+void connection_free_cb(struct ev_loop *loop, struct ev_timer *w, int revents, bool report_incident);
 
 
 void connection_set_nonblocking(struct connection *con);
