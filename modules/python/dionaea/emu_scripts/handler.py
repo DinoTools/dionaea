5dc9863341e231372dbede36316fb5a5ffa36b4e
PhiBo <phibo@dinotools.org>
2016-10-20 21:33:38 +0200

    python/emu_scripts - Add handler to detect powershell

--- modules/python/dionaea/emu_scripts/handler.py
+++ modules/python/dionaea/emu_scripts/handler.py
@@ -1,9 +1,16 @@
+import logging
 import re
+
+logger = logging.getLogger("emu_scripts")
 
 
 class BaseHandler(object):
     name = ""
 
+    def __init__(self, config=None):
+        self._config = {}
+        if isinstance(config, dict):
+            self._config = config
 
 class RawURL(object):
     name = "raw_url"
@@ -18,6 +25,39 @@ class RawURL(object):
         )
 
     def run(self, data):
+        urls = []
+        for m in self._regex_url.finditer(data):
+            urls.append(m.group("url"))
+        return urls
+
+
+class PowerShell(BaseHandler):
+    name = "powershell"
+
+    def __init__(self, config=None):
+        BaseHandler.__init__(self, config=config)
+
+        self._regex_detect = [
+            re.compile(b"New-Object\s+System\.Net\.WebClient"),
+            re.compile(b"DownloadFile([^,]+?,[^,]+?)"),
+            re.compile(b"Invoke-Expression([^)]+?)")
+        ]
+
+        self._regex_url = re.compile(
+            b"\w+\s*=\s*\"(?P<url>(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?)\""
+        )
+
+    def run(self, data):
+        match_count = 0
+        for regex in self._regex_detect:
+            m = regex.search(data)
+            if m:
+                match_count += 1
+
+        if match_count < 2:
+            logger.info("Match count is %d should at least be %d", match_count, 2)
+            return
+
         urls = []
         for m in self._regex_url.finditer(data):
             urls.append(m.group("url"))
