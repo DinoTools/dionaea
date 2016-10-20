cf088fda5069a64e5fe7e3a3cd0d2b00a06867e6
PhiBo <phibo@dinotools.org>
2016-10-20 22:09:57 +0200

    python/emu_scripts - Improve powershell handler

--- modules/python/dionaea/emu_scripts/handler.py
+++ modules/python/dionaea/emu_scripts/handler.py
@@ -62,6 +62,7 @@ class PowerShell(BaseHandler):
     def __init__(self, config=None):
         BaseHandler.__init__(self, config=config)
 
+        self.min_match_count = 2
         self._regex_detect = [
             re.compile(b"New-Object\s+System\.Net\.WebClient"),
             re.compile(b"DownloadFile([^,]+?,[^,]+?)"),
@@ -69,24 +70,8 @@ class PowerShell(BaseHandler):
         ]
 
         self._regex_url = re.compile(
-            b"\w+\s*=\s*\"(?P<url>(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?)\""
+            b"\w+\s*=\s*\"\s*(?P<url>(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?)\s*\""
         )
-
-    def run(self, data):
-        match_count = 0
-        for regex in self._regex_detect:
-            m = regex.search(data)
-            if m:
-                match_count += 1
-
-        if match_count < 2:
-            logger.info("Match count is %d should at least be %d", match_count, 2)
-            return
-
-        urls = []
-        for m in self._regex_url.finditer(data):
-            urls.append(m.group("url"))
-        return urls
 
 
 class VBScript(BaseHandler):
