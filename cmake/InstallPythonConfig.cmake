ac3ae667ce0789c1fe937b7b299b70e7a8a11d64
PhiBo <phibo@dinotools.org>
2018-05-21 22:28:15 +0200

    build - Fix install paths to be more flexible

--- cmake/InstallPythonConfig.cmake
+++ cmake/InstallPythonConfig.cmake
@@ -15,12 +15,12 @@ function(install_available_python_config)
             )
             install_if_not_exists(
                 "${CMAKE_CURRENT_BINARY_DIR}/conf/${MY_FUNC_BUILD_DIR}/${_new_filename}"
-		"${CMAKE_INSTALL_FULL_SYSCONFDIR}/dionaea/${MY_FUNC_DESTINATION_DIR}"
+		"${DIONAEA_CONFDIR}/${MY_FUNC_DESTINATION_DIR}"
             )
         else()
             install_if_not_exists(
                 "${PROJECT_SOURCE_DIR}/conf/${MY_FUNC_SOURCE_DIR}/${_filename}"
-		"${CMAKE_INSTALL_FULL_SYSCONFDIR}/dionaea/${MY_FUNC_DESTINATION_DIR}"
+		"${DIONAEA_CONFDIR}/${MY_FUNC_DESTINATION_DIR}"
             )
         endif()
     endforeach()
@@ -33,17 +33,17 @@ function(install_enabled_python_config)
     cmake_parse_arguments(MY_FUNC "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
 
     if(CMAKE_HOST_UNIX)
-        set(_conf_dst "${CMAKE_INSTALL_FULL_SYSCONFDIR}/dionaea/${MY_FUNC_DESTINATION}")
+        set(_conf_dst "${DIONAEA_CONFDIR}/${MY_FUNC_DESTINATION}")
         if(NOT EXISTS "${_conf_dst}")
             install(DIRECTORY DESTINATION "${_conf_dst}")
             foreach(filename ${MY_FUNC_FILES})
-                install(CODE "message(STATUS \"Enabling Service: ${filename} in ${_conf_dst}\")")
+                install(CODE "message(STATUS \"Enabling Service: ${filename} in \$ENV{DESTDIR}\${CMAKE_INSTALL_PREFIX}/${_conf_dst}\")")
                 install(CODE "
                 EXECUTE_PROCESS(
                     COMMAND \"${CMAKE_COMMAND}\" -E create_symlink
                         ${MY_FUNC_SOURCE_REL_DIR}/${filename}
                         ${filename}
-                    WORKING_DIRECTORY \"${_conf_dst}\"
+                    WORKING_DIRECTORY \"\$ENV{DESTDIR}\${CMAKE_INSTALL_PREFIX}/${_conf_dst}\"
                 )
             ")
             endforeach()
