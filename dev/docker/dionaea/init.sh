2e690a7cbdb4e5c0e385211a48b1f34405ca69cd
PhiBo <phibo@dinotools.org>
2020-10-14 23:11:49 +0200

    dev/docker - Add license info

--- dev/docker/dionaea/init.sh
+++ dev/docker/dionaea/init.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+# This file is part of the dionaea honeypot
+#
+# SPDX-FileCopyrightText: 2020 PhiBo (DinoTools)
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+
 SRC_DIR=$(dirname $0)
 test ! -d /opt/dionaea && (${SRC_DIR}/build.sh || exit 1)
 find /etc/supervisor/conf.d/ -type f -name '*.conf' -exec rm {} \;
